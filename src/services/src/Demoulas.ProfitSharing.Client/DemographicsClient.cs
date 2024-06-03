c49479d2c68fe7f393ec0e6920da6af066833fb9
Russ Harding <russ.harding@ryan.com>
2024-06-03 07:46:33 -0400

    Stub DemographicsClient, doesn't work, just impliments interface

---
+++ src/services/src/Demoulas.ProfitSharing.Client/DemographicsClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Json;
+using Demoulas.Common.Contracts.Request;
+using Demoulas.Common.Contracts.Response;
+using Demoulas.ProfitSharing.Common.Contracts.Request;
+using Demoulas.ProfitSharing.Common.Contracts.Response;
+using Demoulas.ProfitSharing.Common.Interfaces;
+
+namespace Demoulas.ProfitSharing.Client;
+public class DemographicsClient : IDemographicsService
+{
+    private readonly HttpClient _httpClient;
+
+    public DemographicsClient(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient(Constants.HttpClient);
+    }
+
+    public Task<PaginatedResponseDto<DemographicsResponseDto>> GetAllDemographics(PaginationRequestDto req, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ISet<DemographicsResponseDto>> AddDemographics(IEnumerable<DemographicsRequestDto> demographics, CancellationToken cancellationToken)
+    {
+        var response = await _httpClient.PutAsJsonAsync("lookup/payclassification/all", demographics, cancellationToken);
+
+        response.EnsureSuccessStatusCode();
+
+        return new HashSet<DemographicsResponseDto>(0);
+    }
+}
