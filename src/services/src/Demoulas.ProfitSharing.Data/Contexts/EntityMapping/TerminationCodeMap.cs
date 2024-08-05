ef1c78b28e60d5fa1c233567ddf367a31f248652
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2024-08-05 17:14:27 -0400

    PS-222 payben redesign

--- src/services/src/Demoulas.ProfitSharing.Data/Contexts/EntityMapping/TerminationCodeMap.cs
+++ src/services/src/Demoulas.ProfitSharing.Data/Contexts/EntityMapping/TerminationCodeMap.cs
@@ -33,7 +33,21 @@ internal sealed class TerminationCodeMap : IEntityTypeConfiguration<TerminationC
             new TerminationCode { Id = TerminationCode.Constants.HealthReasonsNonFMLA, Name = "Health Reasons Non-FMLA" },
             new TerminationCode { Id = TerminationCode.Constants.LayoffNoWork, Name = "Layoff No Work" },
             new TerminationCode { Id = TerminationCode.Constants.Military, Name = "Military" },
-            new TerminationCode { Id = TerminationCode.Constants.SchoolOrSports, Name = "School Or Sports" }
-        );
+            new TerminationCode { Id = TerminationCode.Constants.SchoolOrSports, Name = "School Or Sports" },
+            new TerminationCode { Id = TerminationCode.Constants.MoveOutOfArea, Name = "Move Out of Area" },
+            new TerminationCode { Id = TerminationCode.Constants.PoorPerformance, Name = "Poor Performance" },
+            new TerminationCode { Id = TerminationCode.Constants.OffForSummer, Name = "Off For Summer" },
+            new TerminationCode { Id = TerminationCode.Constants.WorkmansCompensation, Name = "Workmans Compensation" },
+            new TerminationCode { Id = TerminationCode.Constants.Injured, Name = "Injured" },
+            new TerminationCode { Id = TerminationCode.Constants.Transferred, Name = "Transferred" },
+            new TerminationCode { Id = TerminationCode.Constants.Retired, Name = "Retired" },
+            new TerminationCode { Id = TerminationCode.Constants.Competition, Name = "Competition" },
+            new TerminationCode { Id = TerminationCode.Constants.AnotherJob, Name = "Another Job" },
+            new TerminationCode { Id = TerminationCode.Constants.WouldNotRehire, Name = "Would Not Rehire" },
+            new TerminationCode { Id = TerminationCode.Constants.NeverReported, Name = "Never Reported" },
+            new TerminationCode { Id = TerminationCode.Constants.RetiredReceivingPension, Name = "Retired Receiving Pension" },
+            new TerminationCode { Id = TerminationCode.Constants.FMLAApproved, Name = "FMLA Approved" },
+            new TerminationCode { Id = TerminationCode.Constants.Deceased, Name = "Deceased" }
+    );
     }
 }
--- src/services/src/Demoulas.ProfitSharing.Data/Contexts/ProfitSharingDbContext.cs
+++ src/services/src/Demoulas.ProfitSharing.Data/Contexts/ProfitSharingDbContext.cs
@@ -4,7 +4,6 @@ using Demoulas.ProfitSharing.Data.Entities.MassTransit;
 using Demoulas.ProfitSharing.Data.Extensions;
 using Demoulas.ProfitSharing.Data.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace Demoulas.ProfitSharing.Data.Contexts;
 
@@ -14,9 +13,8 @@ public class ProfitSharingDbContext : OracleDbContext<ProfitSharingDbContext>, I
     {
         // Used for Unit testing/Mocking only
     }
-
     public ProfitSharingDbContext(DbContextOptions<ProfitSharingDbContext> options)
-        : base(options)
+    : base(options)
     {
 
     }
@@ -24,14 +22,17 @@ public class ProfitSharingDbContext : OracleDbContext<ProfitSharingDbContext>, I
     public virtual DbSet<Demographic> Demographics { get; set; }
     public virtual DbSet<Country> Countries { get; set; }
     public virtual DbSet<PayClassification> PayClassifications { get; set; }
-
     public virtual DbSet<ProfitDetail> ProfitDetails { get; set; }
     public virtual DbSet<ProfitCode> ProfitCodes { get; set; }
     public virtual DbSet<TaxCode> TaxCodes { get; set; }
-
     public virtual DbSet<Beneficiary> Beneficiaries { get; set; }
     public virtual DbSet<PayProfit> PayProfits { get; set; }
+
     public DbSet<Job> Jobs { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
+    public virtual DbSet<BeneficiaryType> BeneficiaryTypes { get; set; }
+    public virtual DbSet<EmployeeType> EmployeeTypes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
