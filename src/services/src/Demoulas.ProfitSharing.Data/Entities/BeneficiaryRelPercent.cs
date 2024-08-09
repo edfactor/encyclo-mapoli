48bd2a16228ed1b64e305453b8dd47fd1f77aba8
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2024-08-09 12:46:28 -0400

    Add Table BENEFICARY_REL_PERCENT / PAYBEN

---
+++ src/services/src/Demoulas.ProfitSharing.Data/Entities/BeneficiaryRelPercent.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Demoulas.ProfitSharing.Data.Entities;
+public sealed class BeneficiaryRelPercent
+{
+    public required long PSN { get; set; }
+    public required long SSN { get; set; }
+    public required char KindId { get; set; }
+    public required BeneficiaryKind Kind { get; set; }
+    public required decimal Percent { get; set; }
+    public string? Relationship { get; set; }
+}
