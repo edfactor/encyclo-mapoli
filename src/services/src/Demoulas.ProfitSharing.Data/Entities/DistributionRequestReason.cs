1c2e9092a51e894a49381ca823c993d227a4a568
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2024-08-27 15:16:53 -0400

    Apply code review feedback

--- src/services/src/Demoulas.ProfitSharing.Data/Entities/DistributionRequestReason.cs
+++ src/services/src/Demoulas.ProfitSharing.Data/Entities/DistributionRequestReason.cs
@@ -1,18 +1,32 @@
-﻿using Demoulas.ProfitSharing.Data.Interfaces;
+﻿using System.Collections.Immutable;
+using Demoulas.ProfitSharing.Common.Interfaces;
 
 namespace Demoulas.ProfitSharing.Data.Entities;
 
 public sealed class DistributionRequestReason : ILookupTable<byte>
 {
-    public static List<string> Reasons = new List<string> {
-        "HOME_PURCHASE",
-        "HOME_REPAIR",
-        "CAR",
-        "EDUCATION_EXP",
-        "FUNERAL_EXP",
-        "MEDICAL_DENTAL",
-        "EVICTION_OR_FORECLOSE",
-        "OTHER" };
+    public static class Constants
+    {
+        public const string HOME_PURCHASE = "HOME_PURCHASE";
+        public const string HOME_REPAIR = "HOME_REPAIR";
+        public const string CAR = "CAR";
+        public const string EDUCATION_EXP = "EDUCATION_EXP";
+        public const string FUNERAL_EXP = "FUNERAL_EXP";
+        public const string MEDICAL_DENTAL = "MEDICAL_DENTAL";
+        public const string EVICTION_OR_FORECLOSE = "EVICTION_OR_FORECLOSE";
+        public const string OTHER = "OTHER";
+    }
+
+    public static readonly ImmutableList<string> Reasons = ImmutableList.Create(
+        Constants.HOME_PURCHASE,
+        Constants.HOME_REPAIR,
+        Constants.CAR,
+        Constants.EDUCATION_EXP,
+        Constants.FUNERAL_EXP,
+        Constants.MEDICAL_DENTAL,
+        Constants.EVICTION_OR_FORECLOSE,
+        Constants.OTHER
+    );
 
     public byte Id { get; set; }
     public required string Name { get; set; }
--- src/services/src/Demoulas.ProfitSharing.Data/Entities/DistributionRequestType.cs
+++ src/services/src/Demoulas.ProfitSharing.Data/Entities/DistributionRequestType.cs
@@ -1,16 +1,27 @@
-﻿using Demoulas.ProfitSharing.Data.Interfaces;
+﻿using System.Collections.Immutable;
+using Demoulas.ProfitSharing.Common.Interfaces;
 
 namespace Demoulas.ProfitSharing.Data.Entities;
 
 public class DistributionRequestType : ILookupTable<byte>
 {
-    public static List<string> Types = new List<string> {
-        "HARDSHIP",
-        "YEARLY",
-        "ONE_TIME",
-        "PAYOUT",
-        "ROLLOVER",
-     };
+
+    public static class Constants
+    {
+        public const string HARDSHIP = "HARDSHIP";
+        public const string YEARLY = "YEARY";
+        public const string ONE_TIME = "ONE_TIME";
+        public const string PAYOUT = "PAYOUT";
+        public const string ROLLOVER = "ROLLOVER";
+    }
+
+    public static readonly ImmutableList<string> Types = ImmutableList.Create(
+        Constants.HARDSHIP,
+        Constants.YEARLY,
+        Constants.ONE_TIME,
+        Constants.PAYOUT,
+        Constants.ROLLOVER
+    );
 
     public byte Id { get; set; }
     public required string Name { get; set; }
