13558ff4ce03df69ac5852eb42b55760c5505ba1
Russ Harding <rharding@mainoffice.demoulas.corp>
2024-09-13 13:31:35 -0400

    Add possible error responses

--- src/services/src/Demoulas.ProfitSharing.Endpoints/Groups/PayProfitGroup.cs
+++ src/services/src/Demoulas.ProfitSharing.Endpoints/Groups/PayProfitGroup.cs
@@ -1,4 +1,5 @@
-﻿using FastEndpoints;
+﻿using System.Net;
+using FastEndpoints;
 using Microsoft.AspNetCore.Http;
 
 namespace Demoulas.ProfitSharing.Endpoints.Groups;
@@ -12,8 +13,16 @@ public sealed class PayProfitGroup : Group
         Configure(Route.ToLowerInvariant(), ep => //admin is the route prefix for the top level group
         {
             ep.Description(x => x
+                .Produces((int)HttpStatusCode.Unauthorized)
+                .Produces((int)HttpStatusCode.Forbidden)
+                .Produces((int)HttpStatusCode.RequestTimeout)
+                .Produces((int)HttpStatusCode.TooManyRequests)
                 .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>()
-                .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>(500)
+                .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.NotAcceptable)
+                .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.NotFound)
+                .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.UnprocessableEntity)
+                .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.MethodNotAllowed)
+                .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.InternalServerError)
                 .WithTags(RouteName));
         });
     }
--- src/services/src/Demoulas.ProfitSharing.Endpoints/Groups/YearEndGroup.cs
+++ src/services/src/Demoulas.ProfitSharing.Endpoints/Groups/YearEndGroup.cs
@@ -15,10 +15,14 @@ public sealed class YearEndGroup : Group
         Configure(Route.ToLowerInvariant(), ep => //admin is the route prefix for the top level group
         {
             ep.Description(x => x
-                .Produces(401)
-                .Produces(403)
-                .Produces(429)
+                .Produces((int)HttpStatusCode.Unauthorized)
+                .Produces((int)HttpStatusCode.Forbidden)
+                .Produces((int)HttpStatusCode.RequestTimeout)
+                .Produces((int)HttpStatusCode.TooManyRequests)
                 .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>()
+                .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.NotAcceptable)
+                .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.NotFound)
+                .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.UnprocessableEntity)
                 .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.MethodNotAllowed)
                 .ProducesProblemFE<Microsoft.AspNetCore.Mvc.ProblemDetails>((int)HttpStatusCode.InternalServerError)
                 .WithRequestTimeout(TimeSpan.FromMinutes(1))
