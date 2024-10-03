a67e0b6ec7739a8c0a1fbe3ca3e6e04a8b9ac9ab
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2024-10-03 12:04:25 -0400

    Add in integration test

---
+++ src/services/tests/Demoulas.ProfitSharing.IntegrationTests/Reports/YearEnd/ApiIntegrationTestBase.cs
@@ -0,0 +1,84 @@
+﻿using Demoulas.Common.Contracts.Contracts.Response;
+using Demoulas.ProfitSharing.Common.Contracts.Response.YearEnd;
+using Demoulas.ProfitSharing.Data.Contexts;
+using Demoulas.ProfitSharing.Data.Interfaces;
+using Demoulas.ProfitSharing.UnitTests.Mocks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Demoulas.ProfitSharing.IntegrationTests.Reports.YearEnd;
+
+/**
+ * WARNING - needs a refactor
+ *
+ * This class should be refactored to have a proper database connection to the pristine obfuscated database.
+ *
+ * now it uses a mock to get the testing framework to swap in the read only connection.
+ *
+ */
+
+/// <summary>
+///   Abstraction for testing api endpoints that use a <c>DbContext</c>.
+///   sets up a web application, endpoints and services and a real read only database connection.
+/// </summary>
+public class ApiIntegrationTestBase<TStartup> where TStartup : class
+{
+    /// <summary>
+    ///    Not quite mock, this factory has a ready only connection to the read only version of the SMART obfuscated pristine database.
+    /// </summary>
+    public IProfitSharingDataContextFactory SomeWhatMockDbContextFactory { get; }
+
+    /// <summary>
+    ///   The client to invoke api endpoints.
+    /// </summary>
+    public HttpClient ApiClient { get; }
+
+    public HttpClient DownloadClient { get; }
+
+    public ServiceProvider? ServiceProvider { get; private set; }
+
+    /// <summary>
+    ///   Initializes a default instance of <c>ApiIntegrationTestBase</c>
+    /// </summary>
+    /// <remarks>
+    ///   Intentionally set as <c>protected</c> to prevent instances being created.
+    /// </remarks>
+    protected ApiIntegrationTestBase()
+    {
+        // We get a connection to the SMART obfuscated pristine database.
+        var configuration = new ConfigurationBuilder().AddUserSecrets<TStartup>().Build();
+        string connectionString = configuration["ConnectionStrings:ProfitSharing-ObfuscatedPristine"]!;
+        var options = new DbContextOptionsBuilder<ProfitSharingReadOnlyDbContext>().UseOracle(connectionString).EnableSensitiveDataLogging().Options;
+        var ctx = new ProfitSharingReadOnlyDbContext(options);
+
+        // Probably should simply use a normal database connection
+        var profitSharingDataContextFactoryMock = new Mock<IProfitSharingDataContextFactory>();
+
+        profitSharingDataContextFactoryMock
+            .Setup(factory => factory.UseReadOnlyContext(It.IsAny<Func<ProfitSharingReadOnlyDbContext, Task<PaginatedResponseDto<ExecutiveHoursAndDollarsResponse>>>>()))
+            .Returns<Func<ProfitSharingReadOnlyDbContext, Task<PaginatedResponseDto<ExecutiveHoursAndDollarsResponse>>>>(func => func(ctx));
+
+        SomeWhatMockDbContextFactory = profitSharingDataContextFactoryMock.Object;
+
+        WebApplicationFactory<TStartup> webApplicationFactory = new WebApplicationFactory<TStartup>();
+
+        WebApplicationFactory<TStartup> builder = webApplicationFactory.WithWebHostBuilder(
+            hostBuilder =>
+            {
+                hostBuilder.UseEnvironment("Testing");
+                hostBuilder.ConfigureServices(services =>
+                {
+                    services.AddTransient((c) => SomeWhatMockDbContextFactory);
+                    ServiceProvider = services.BuildServiceProvider();
+                });
+            });
+
+        ApiClient = builder.CreateClient();
+        DownloadClient = builder.CreateClient();
+        DownloadClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/csv"));
+    }
+}
