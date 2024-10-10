4ed71f98fea14a2ca806628b5587973616e34a93
Dakota Kim <dkim@demoulasmarketbasket.com>
2024-10-10 08:45:59 -0400

    add new routes

--- src/ui/src/components/router/Router.tsx
+++ src/ui/src/components/router/Router.tsx
@@ -5,9 +5,11 @@ import { MenuBar } from "smart-ui-library";
 import MenuData from "../../MenuData";
 
 import ImpersonationMultiSelect from "components/MenuBar/ImpersonationMultiSelect";
+import DemographicBadgesNotInPayprofit from "pages/DemographicBadgesNotInPayprofit/DemographicBadgesNotInPayprofit";
+import DuplicateSSNsOnDemographics from "pages/DuplicateSSNsOnDemographics/DuplicateSSNsOnDemographics";
 
 const Router = () => {
-  const oktaEnabled = import.meta.env.VITE_REACT_APP_OKTA_ENABLED == 'true';
+  const oktaEnabled = import.meta.env.VITE_REACT_APP_OKTA_ENABLED == "true";
   const isProduction = false;
   const hasImpersonationRole = true;
   const showImpersonation = hasImpersonationRole && !isProduction;
@@ -20,6 +22,14 @@ const Router = () => {
           path=""
           index={true}
           element={<LandingPage />}></Route>
+        <Route
+          path="demographic-badges-not-in-payprofit"
+          index={true}
+          element={<DemographicBadgesNotInPayprofit />}></Route>
+        <Route
+          index={true}
+          path="duplicate-ssns-demographics"
+          element={<DuplicateSSNsOnDemographics />}></Route>
       </RouteSecurity>
     </BrowserRouter>
   );
