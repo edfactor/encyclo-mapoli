b021ea57d441146ea589b089aa4adc27f5a1ec77
Dakota Kim <dkim@demoulasmarketbasket.com>
2024-10-10 08:46:23 -0400

    Duplicate SSNs on Demographics page

---
+++ src/ui/src/pages/DuplicateSSNsOnDemographics/DuplicateSSNsOnDemographicsGrid.tsx
@@ -0,0 +1,60 @@
+import { Typography } from "@mui/material";
+import { useState, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLazyGetDuplicateSSNsQuery } from "reduxstore/api/YearsEndApi";
+import { RootState } from "reduxstore/store";
+import { DSMGrid, ISortParams, Pagination } from "smart-ui-library";
+import { GetDuplicateSSNsOnDemographicsColumns } from "./DuplicateSSNsOnDemographicsGridColumns";
+
+const DuplicateSSNsOnDemographicsGrid = () => {
+  const [pageNumber, setPageNumber] = useState(0);
+  const [pageSize, setPageSize] = useState(25);
+  const [sortParams, setSortParams] = useState<ISortParams>({
+    sortBy: "Badge",
+    isSortDescending: false
+  });
+
+  const dispatch = useDispatch();
+  const { duplicateSSNsData } = useSelector((state: RootState) => state.yearsEnd)
+  const [_, { isLoading }] = useLazyGetDuplicateSSNsQuery();
+
+  const sortEventHandler = (update: ISortParams) => setSortParams(update);
+  const columnDefs = useMemo(() => GetDuplicateSSNsOnDemographicsColumns(), []);
+
+  return (
+    <>
+      <div style={{ padding: "0 24px 0 24px" }}>
+        <Typography
+          variant="h2"
+          sx={{ color: "#0258A5" }}>
+          {`PAYROLL DUPLICATE SSNs ON DEMOGRAPHICS (${duplicateSSNsData?.response.total || 0})`}
+        </Typography>
+      </div>
+      <DSMGrid
+        preferenceKey={"DUPE_SSNS"}
+        isLoading={isLoading}
+        handleSortChanged={sortEventHandler}
+        providedOptions={{
+          rowData: duplicateSSNsData?.response.results,
+          columnDefs: columnDefs
+        }}
+      />
+      {!!duplicateSSNsData && duplicateSSNsData.response.results.length > 0 && (
+        <Pagination
+          pageNumber={pageNumber}
+          setPageNumber={(value: number) => {
+            setPageNumber(value - 1);
+          }}
+          pageSize={pageSize}
+          setPageSize={(value: number) => {
+            setPageSize(value);
+            setPageNumber(1);
+          }}
+          recordCount={duplicateSSNsData.response.total}
+        />
+      )}
+    </>
+  );
+};
+
+export default DuplicateSSNsOnDemographicsGrid;
