04db4516137e86d77a7063fcf2df4507629c57c6
Russ Harding <rharding@mainoffice.demoulas.corp>
2024-10-10 08:42:24 -0400

    Inherit from ProfitYearRequest

--- src/services/src/Demoulas.ProfitSharing.Common/Contracts/Request/TerminatedEmployeeAndBeneficiaryDataRequest.cs
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Request/TerminatedEmployeeAndBeneficiaryDataRequest.cs
@@ -3,12 +3,10 @@ using Demoulas.Common.Contracts.Contracts.Request;
 
 namespace Demoulas.ProfitSharing.Common.Contracts.Request;
 
-public sealed record TerminatedEmployeeAndBeneficiaryDataRequest : PaginationRequestDto
+public sealed record TerminatedEmployeeAndBeneficiaryDataRequest : ProfitYearRequest
 {
     [DefaultValue("01/07/2023")]
     public required DateOnly StartDate { get; set; }
     [DefaultValue("01/02/2024")]
     public required DateOnly EndDate { get; set; }
-    [DefaultValue("2023.0")]
-    public required decimal ProfitShareYear { get; set; }
 }
