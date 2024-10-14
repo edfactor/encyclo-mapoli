46131eaa5dd0087eee8f83b6eec96a9fdf469462
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2024-10-14 16:27:42 -0400

    Merges Set/Get Services.  Adds searching on Get endpoint

---
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Request/ExecutiveHoursAndDollarsRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using Demoulas.Common.Contracts.Contracts.Request;
+
+namespace Demoulas.ProfitSharing.Common.Contracts.Request;
+
+public sealed record ExecutiveHoursAndDollarsRequest : ProfitYearRequest
+{
+    public long? Ssn { get; set; }
+    public int? BadgeNumber { get; set; }
+    public string? FullName { get; set; }
+    
+    public bool? HasExecutiveHoursAndDollars { get; set; }
+
+}
