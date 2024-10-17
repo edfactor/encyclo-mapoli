8dea977bbeffc52955e7a0bff6b986cb385b24d9
Dakota Kim <dkim@demoulasmarketbasket.com>
2024-10-17 06:56:43 -0400

    add slice state + action

--- src/ui/src/reduxstore/slices/yearsEndSlice.ts
+++ src/ui/src/reduxstore/slices/yearsEndSlice.ts
@@ -1,14 +1,16 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { DemographicBadgesNotInPayprofit, DuplicateSSNDetail, PagedReportResponse } from "reduxstore/types";
+import { DemographicBadgesNotInPayprofit, DuplicateNameAndBirthday, DuplicateSSNDetail, PagedReportResponse } from "reduxstore/types";
 
 export interface YearsEndState {
   duplicateSSNsData: PagedReportResponse<DuplicateSSNDetail> | null;
   demographicBadges: PagedReportResponse<DemographicBadgesNotInPayprofit> | null;
+  duplicateNamesAndBirthday: PagedReportResponse<DuplicateNameAndBirthday> | null;
 }
 
 const initialState: YearsEndState = {
     duplicateSSNsData: null,
     demographicBadges: null,
+    duplicateNamesAndBirthday: null
 };
 
 export const yearsEndSlice = createSlice({
@@ -20,8 +22,11 @@ export const yearsEndSlice = createSlice({
       },
     setDemographicBadgesNotInPayprofitData: (state, action: PayloadAction<PagedReportResponse<DemographicBadgesNotInPayprofit>>) => {
         state.demographicBadges = action.payload;
-      }
+    },
+    setDuplicateNamesAndBirthdays: (state, action: PayloadAction<PagedReportResponse<DuplicateNameAndBirthday>>) => {
+      state.duplicateNamesAndBirthday = action.payload;
+  },
   }});
 
-export const { setDuplicateSSNsData, setDemographicBadgesNotInPayprofitData } = yearsEndSlice.actions;
+export const { setDuplicateSSNsData, setDemographicBadgesNotInPayprofitData, setDuplicateNamesAndBirthdays } = yearsEndSlice.actions;
 export default yearsEndSlice.reducer;
