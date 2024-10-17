e26f5cb00a1e76f1cf4b215079cc39c7c87ade9e
Dakota Kim <dkim@demoulasmarketbasket.com>
2024-10-17 06:56:31 -0400

    define types

--- src/ui/src/reduxstore/types.ts
+++ src/ui/src/reduxstore/types.ts
@@ -66,3 +66,35 @@ export interface DuplicateSSNsRequestDto extends ImpersonationRequest {
   profitYear: number;
   pagination: PaginationParams;
 }
+
+
+export interface DuplicateNameAndBirthdayRequestDto extends ImpersonationRequest {
+  profitYear: number;
+  pagination: PaginationParams;
+}
+
+export interface DuplicateNameBirthdayAddress {
+  street: string;
+  street2: string | null;
+  city: string;
+  state: string;
+  postalCode: string;
+  countryIso: string;
+}
+
+export interface DuplicateNameAndBirthday {
+  badgeNumber: number;
+  ssn: string;
+  name: string;
+  dateOfBirth: string;
+  address: DuplicateNameBirthdayAddress;
+  years: number;
+  hireDate: string;
+  terminationDate: string | null;
+  status: string;
+  storeNumber: number;
+  count: number;
+  netBalance: number;
+  hoursCurrentYear: number;
+  incomeCurrentYear: number;
+}
