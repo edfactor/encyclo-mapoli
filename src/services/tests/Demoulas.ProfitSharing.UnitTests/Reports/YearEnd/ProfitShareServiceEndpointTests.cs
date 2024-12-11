de42f1b0599d6f3df265345dcfd81150d51d7f03
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2024-12-11 08:57:57 -0500

    Ensure we always have an employee with points earned

--- src/services/tests/Demoulas.ProfitSharing.UnitTests/Reports/YearEnd/ProfitShareServiceEndpointTests.cs
+++ src/services/tests/Demoulas.ProfitSharing.UnitTests/Reports/YearEnd/ProfitShareServiceEndpointTests.cs
@@ -2,12 +2,16 @@
 using Demoulas.ProfitSharing.Api;
 using Demoulas.ProfitSharing.Common.Contracts.Request;
 using Demoulas.ProfitSharing.Common.Contracts.Response.YearEnd;
+using Demoulas.ProfitSharing.Data.Contexts;
+using Demoulas.ProfitSharing.Data.Entities;
 using Demoulas.ProfitSharing.Endpoints.Endpoints.Reports.YearEnd.ProfitShareUpdate;
 using Demoulas.ProfitSharing.Security;
 using Demoulas.ProfitSharing.UnitTests.Base;
 using Demoulas.ProfitSharing.UnitTests.Extensions;
 using FastEndpoints;
 using FluentAssertions;
+using MassTransit.Courier;
+using Microsoft.EntityFrameworkCore;
 
 namespace Demoulas.ProfitSharing.UnitTests.Reports.YearEnd;
 
@@ -48,23 +52,52 @@ public class ProfitShareServiceEndpointTests : ApiTestBase<Program>
 
 
     [Fact]
-    public async Task Ensure_max_contribution_is_tripped()
+    public void Ensure_max_contribution_is_tripped()
     {
-        // Arrange
-        UpdateAdjustmentAmountsRequest req = new()
-        {
-            ProfitYear = 2024, AdjustContributionAmount = 20, MaxAllowedContributions = 1
-        };
-        ApiClient.CreateAndAssignTokenForClient(Role.FINANCEMANAGER);
+        _ = MockDbContextFactory.UseWritableContext(async c =>
+       {
 
-        // Act
-        TestResult<ProfitShareUpdateResponse> response =
-            await ApiClient
-                .GETAsync<ProfitShareUpdateEndpoint,
-                    UpdateAdjustmentAmountsRequest, ProfitShareUpdateResponse>(req);
+           // Arrange
+           // ensure we always have an employee with PointsEarned
+           await ModifyAnEmployee(c);
+           UpdateAdjustmentAmountsRequest req = new()
+           {
+               ProfitYear = 2024,
+               AdjustContributionAmount = 20,
+               MaxAllowedContributions = 1
+           };
+           ApiClient.CreateAndAssignTokenForClient(Role.FINANCEMANAGER);
 
-        // Assert
-        response.Response.StatusCode.Should().Be(HttpStatusCode.OK);
-        response.Result.IsReRunRequired.Should().BeTrue();
+           // Act
+           TestResult<ProfitShareUpdateResponse> response =
+               await ApiClient
+                   .GETAsync<ProfitShareUpdateEndpoint,
+                       UpdateAdjustmentAmountsRequest, ProfitShareUpdateResponse>(req);
+
+           // Assert
+           response.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+           response.Result.IsReRunRequired.Should().BeTrue();
+
+       });
+      
     }
+
+
+    private static async Task<PayProfit> ModifyAnEmployee(ProfitSharingDbContext c)
+    {
+        PayProfit pp = await c.PayProfits
+            .Include(payProfit => payProfit.Demographic!)
+            .ThenInclude(demographic => demographic.ContactInfo)
+            .Include(p => p.Demographic != null)
+            .FirstAsync();
+        Demographic demo = pp.Demographic!;
+
+        pp.ProfitYear = 2024;
+        pp.PointsEarned = 100_000 / 100;
+
+        await c.SaveChangesAsync();
+        return pp;
+    }
+
+
 }
