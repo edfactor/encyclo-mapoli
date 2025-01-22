90cf5c0ac6eed8961e5f3ee546060236ff3673ae
Russ Harding <rharding@mainoffice.demoulas.corp>
2025-01-22 15:33:15 -0500

    Adjusted grouping logic in the `FrozenReportService` to group only by age and replace EmploymentType with the ReportType value.
    Added a method to execute `npm install` in the application startup to ensure dependencies are installed.

--- src/services/src/Demoulas.ProfitSharing.Services/Reports/FrozenReportService.cs
+++ src/services/src/Demoulas.ProfitSharing.Services/Reports/FrozenReportService.cs
@@ -221,11 +221,11 @@ public class FrozenReportService : IFrozenReportService
             x.Amount,
             x.CommentTypeId
         })
-            .GroupBy(x => new { x.Age, x.EmploymentType })
+            .GroupBy(x => new { x.Age })
             .Select(g => new DistributionsByAgeDetail
             {
                 Age = g.Key.Age,
-                EmploymentType = g.Key.EmploymentType,
+                EmploymentType = req.ReportType.ToString(),
                 EmployeeCount = g.Select(x => x.BadgeNumber).Distinct().Count(),
                 Amount = g.Sum(x => x.Amount),
                 // Compute the total hardship amount within the group
