8fdba723c2735c53d39b5c38a286210e26416ffb
Ed Lyons <elyons@demoulasmarketbasket.com>
2025-01-26 22:58:12 -0500

    Fix import errors

--- src/ui/src/pages/ManageExecutiveHoursAndDollars/ManageExecutiveHoursAndDollars.tsx
+++ src/ui/src/pages/ManageExecutiveHoursAndDollars/ManageExecutiveHoursAndDollars.tsx
@@ -1,12 +1,12 @@
 import { Divider } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import { DSMAccordion, Page } from "smart-ui-library";
-import MilitaryAndRehireSearchFilter from "./ManageExecutiveHoursAndDollarsSearchFilter";
-import MilitaryAndRehireGrid from "./ManageExecutiveHoursAndDollarsGrid";
+import ManageExecutiveHoursAndDollarsSearchFilter from "./ManageExecutiveHoursAndDollarsSearchFilter";
+import ManageExecutiveHoursAndDollarsGrid from "./ManageExecutiveHoursAndDollarsGrid";
 
 const ManageExecutiveHoursAndDollars = () => {
   return (
-    <Page label="Manage Executive Hours And Dollars">
+    <Page label="Manage Edxecutive Hours And Dollars">
         <Grid2
           container
           rowSpacing="24px">
@@ -16,13 +16,13 @@ const ManageExecutiveHoursAndDollars = () => {
           <Grid2
             width={"100%"}>
               <DSMAccordion title="Filter">
-                <MilitaryAndRehireSearchFilter />
+                <ManageExecutiveHoursAndDollarsSearchFilter />
               </DSMAccordion>
              
           </Grid2>
 
           <Grid2 width="100%">
-            <MilitaryAndRehireGrid />
+            <ManageExecutiveHoursAndDollarsGrid />
           </Grid2>
         </Grid2>
     </Page>
