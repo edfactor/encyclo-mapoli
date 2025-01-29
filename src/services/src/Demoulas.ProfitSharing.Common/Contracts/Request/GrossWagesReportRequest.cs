c978f198ce2050fcd669a44767e032a57f2dbc3d
Phil Denoncourt <pdenoncourt@demoulasmarketbasket.com>
2025-01-29 08:40:15 -0500

    PS-404 - Initial implementation

---
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Request/GrossWagesReportRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace Demoulas.ProfitSharing.Common.Contracts.Request;
+public record GrossWagesReportRequest : ProfitYearRequest
+{
+    const int DefaultGrossAmount = 50000;
+
+    [DefaultValue(DefaultGrossAmount)]
+    public decimal MinGrossAmount { get; set; } 
+
+    public static new GrossWagesReportRequest RequestExample()
+    {
+        return new GrossWagesReportRequest()
+        {
+            ProfitYear = 2023,
+            MinGrossAmount = DefaultGrossAmount
+        };
+        
+    }
+}
