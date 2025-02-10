c0d63e4896d501f6f4b7d16a0fca1f3ab81adb51
Ed Lyons <ed.lyons@ryan.com>
2025-02-10 18:49:43 -0500

    Save button needs to be pending-data aware, init submit stuff

--- src/ui/src/pages/ManageExecutiveHoursAndDollars/ManageExecutiveHoursAndDollars.tsx
+++ src/ui/src/pages/ManageExecutiveHoursAndDollars/ManageExecutiveHoursAndDollars.tsx
@@ -1,32 +1,39 @@
 import { Divider } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import { DSMAccordion, Page } from "smart-ui-library";
-import PauseCircleOutlineOutlinedIcon from "@mui/icons-material/PauseCircleOutlineOutlined";
-import { Button, Tooltip  } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 import ManageExecutiveHoursAndDollarsSearchFilter from "./ManageExecutiveHoursAndDollarsSearchFilter";
 import ManageExecutiveHoursAndDollarsGrid from "./ManageExecutiveHoursAndDollarsGrid";
-import { FolderOutlined, SaveOutlined } from "@mui/icons-material";
+import { SaveOutlined } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "reduxstore/store";
+import { clearExecutiveHoursAndDollarsGridRows } from "reduxstore/slices/yearsEndSlice";
 
-
-const renderSaveButton = () => {
-  const canSave = true;
-
+const RenderSaveButton = (pendingChanges: boolean) => {
+  const dispatch = useDispatch();
   const saveButton = (
     <Button
-      disabled={!canSave}
+      disabled={!pendingChanges}
       variant="outlined"
       color="primary"
       size="medium"
-      startIcon={<SaveOutlined color={canSave ? "primary" : "disabled"} />}
+      startIcon={<SaveOutlined color={pendingChanges ? "primary" : "disabled"} />}
       onClick={async () => {
-        //await saveDeposit({ ardId: currentArdId ?? 0 });
-        //navigate("/charges/payments/payment-confirmation");
+        // What do we need here?
+        // 1. We need to do the submit via a Redux api call
+        // 2. If fail, show an error and leave the grid unaltered
+        // 3. If success, clear the pending changes of the grid
+        // 4. Show some sort of confirmation message?
+
+        // Note that clearing the rows will also disable the save button,
+        // which will be notified that there are no pending rows to save
+        dispatch(clearExecutiveHoursAndDollarsGridRows());
       }}>
       Save
     </Button>
   );
 
-  if (!canSave) {
+  if (!pendingChanges) {
     return (
       <Tooltip
         placement="top"
@@ -37,34 +44,37 @@ const renderSaveButton = () => {
   } else {
     return saveButton;
   }
-}
+};
 const ManageExecutiveHoursAndDollars = () => {
+  const { executiveHoursAndDollarsGrid } = useSelector((state: RootState) => state.yearsEnd);
+
+  const pendingChanges =
+    executiveHoursAndDollarsGrid !== undefined &&
+    executiveHoursAndDollarsGrid?.executiveHoursAndDollars !== undefined &&
+    executiveHoursAndDollarsGrid?.executiveHoursAndDollars.length != 0;
+
   return (
-    <Page 
+    <Page
       label="Manage Executive Hours And Dollars"
       actionNode={
-        <div style={{ gap: "24px", display: "flex", justifyContent: "end" }}>
-          {renderSaveButton()}
-        </div>
+        <div style={{ gap: "24px", display: "flex", justifyContent: "end" }}>{RenderSaveButton(pendingChanges)}</div>
       }>
-        <Grid2
-          container
-          rowSpacing="24px">
-          <Grid2 width={"100%"}>
-            <Divider />
-          </Grid2>
-          <Grid2
-            width={"100%"}>
-              <DSMAccordion title="Filter">
-                <ManageExecutiveHoursAndDollarsSearchFilter />
-              </DSMAccordion>
-             
-          </Grid2>
-
-          <Grid2 width="100%">
-            <ManageExecutiveHoursAndDollarsGrid />
-          </Grid2>
+      <Grid2
+        container
+        rowSpacing="24px">
+        <Grid2 width={"100%"}>
+          <Divider />
         </Grid2>
+        <Grid2 width={"100%"}>
+          <DSMAccordion title="Filter">
+            <ManageExecutiveHoursAndDollarsSearchFilter />
+          </DSMAccordion>
+        </Grid2>
+
+        <Grid2 width="100%">
+          <ManageExecutiveHoursAndDollarsGrid />
+        </Grid2>
+      </Grid2>
     </Page>
   );
 };
