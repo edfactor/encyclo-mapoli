b746a26c8f27f738b171e2a6ba4232731437aed3
Russ Harding <rharding@mainoffice.demoulas.corp>
2025-02-14 11:51:55 -0500

    experiment

--- src/services/src/Demoulas.ProfitSharing.Services/Military/MilitaryService.cs
+++ src/services/src/Demoulas.ProfitSharing.Services/Military/MilitaryService.cs
@@ -1,25 +1,27 @@
-﻿using Demoulas.ProfitSharing.Data.Interfaces;
-using Microsoft.EntityFrameworkCore;
+﻿#pragma warning disable S125
+//using Demoulas.ProfitSharing.Data.Interfaces;
+#pragma warning restore S125
+//using Microsoft.EntityFrameworkCore;
 
-namespace Demoulas.ProfitSharing.Services.Military
-{
-    public class MilitaryService
-    {
-        private readonly IProfitSharingDataContextFactory _dataContextFactory;
+//namespace Demoulas.ProfitSharing.Services.Military
+//{
+//    public class MilitaryService
+//    {
+//        private readonly IProfitSharingDataContextFactory _dataContextFactory;
 
-        public MilitaryService(IProfitSharingDataContextFactory dataContextFactory)
-        {
-            _dataContextFactory = dataContextFactory;
-        }
+//        public MilitaryService(IProfitSharingDataContextFactory dataContextFactory)
+//        {
+//            _dataContextFactory = dataContextFactory;
+//        }
 
-        public async Task<MilitaryServiceRecord> GetMilitaryServiceRecordAsync(int employeeId)
-        {
-            using var context = _dataContextFactory.UseReadOnlyContext(c =>
-            {
-                c.ProfitDetails
-                    .FirstOrDefaultAsync(x => x.EmployeeId == employeeId);
-            });
-        }
-    }
-    }
-}
+//        public async Task<MilitaryServiceRecord> GetMilitaryServiceRecordAsync(int employeeId)
+//        {
+//            using var context = _dataContextFactory.UseReadOnlyContext(c =>
+//            {
+//                c.ProfitDetails
+//                    .FirstOrDefaultAsync(x => x.EmployeeId == employeeId);
+//            });
+//        }
+//    }
+//    }
+//}
