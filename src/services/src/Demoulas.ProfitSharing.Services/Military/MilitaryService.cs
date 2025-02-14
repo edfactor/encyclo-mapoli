9364449f658d384b052c03be581788f356f439ff
Russ Harding <rharding@mainoffice.demoulas.corp>
2025-02-14 08:50:38 -0500

    Remove unnecessary overload

---
+++ src/services/src/Demoulas.ProfitSharing.Services/Military/MilitaryService.cs
@@ -0,0 +1,25 @@
+﻿using Demoulas.ProfitSharing.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demoulas.ProfitSharing.Services.Military
+{
+    public class MilitaryService
+    {
+        private readonly IProfitSharingDataContextFactory _dataContextFactory;
+
+        public MilitaryService(IProfitSharingDataContextFactory dataContextFactory)
+        {
+            _dataContextFactory = dataContextFactory;
+        }
+
+        public async Task<MilitaryServiceRecord> GetMilitaryServiceRecordAsync(int employeeId)
+        {
+            using var context = _dataContextFactory.UseReadOnlyContext(c =>
+            {
+                c.ProfitDetails
+                    .FirstOrDefaultAsync(x => x.EmployeeId == employeeId);
+            });
+        }
+    }
+    }
+}
