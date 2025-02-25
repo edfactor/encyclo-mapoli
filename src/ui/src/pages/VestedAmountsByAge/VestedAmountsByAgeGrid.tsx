ccd47e264f1874ea48a428bb7ab0a8aebadb9778
Ed Lyons <ed.lyons@ryan.com>
2025-02-25 17:49:42 -0500

    Cleanup

--- src/ui/src/pages/VestedAmountsByAge/VestedAmountsByAgeGrid.tsx
+++ src/ui/src/pages/VestedAmountsByAge/VestedAmountsByAgeGrid.tsx
@@ -5,10 +5,21 @@ import { useLazyGetVestingAmountByAgeQuery } from "reduxstore/api/YearsEndApi";
 import { RootState } from "reduxstore/store";
 import { DSMGrid, ISortParams } from "smart-ui-library";
 import { GetVestedAmountsByAgeColumns } from "./VestedAmountsByAgeGridColumns";
-import Grid2 from "@mui/material/Unstable_Grid2";
 import { VestedAmountsByAge } from "../../reduxstore/types";
 
-const VestedAmountsByAgeGrid = () => {
+interface VestedAmountsByAgeGridProps {
+  gridTitle: string;
+  countColName: string;
+  amountColName: string;
+  totalCount?: keyof VestedAmountsByAge | undefined;
+}
+
+const VestedAmountsByAgeGrid: React.FC<VestedAmountsByAgeGridProps> = ({
+  gridTitle,
+  amountColName,
+  countColName,
+  totalCount
+}) => {
   const [_sortParams, setSortParams] = useState<ISortParams>({
     sortBy: "Badge",
     isSortDescending: false
@@ -19,66 +30,7 @@ const VestedAmountsByAgeGrid = () => {
 
   const sortEventHandler = (update: ISortParams) => setSortParams(update);
 
-  const columnDefsTotal = GetVestedAmountsByAgeColumns();
-
-  const createSummaryRows = (data: VestedAmountsByAge) => [
-    {
-      reportName: data?.reportName || "Summary",
-      totalFullTime100PercentAmount: data?.totalFullTime100PercentAmount || 0,
-      totalFullTimePartialAmount: data?.totalFullTimePartialAmount || 0,
-      totalFullTimeNotVestedAmount: data?.totalFullTimeNotVestedAmount || 0,
-      totalPartTime100PercentAmount: data?.totalPartTime100PercentAmount || 0,
-      totalPartTimePartialAmount: data?.totalPartTimePartialAmount || 0,
-      totalPartTimeNotVestedAmount: data?.totalPartTimeNotVestedAmount || 0,
-      totalBeneficiaryCount: data?.totalBeneficiaryCount || 0,
-      totalBeneficiaryAmount: data?.totalBeneficiaryAmount || 0,
-      totalFullTimeCount: data?.totalFullTimeCount || 0,
-      totalNotVestedCount: data?.totalNotVestedCount || 0,
-      totalPartialVestedCount: data?.totalPartialVestedCount || 0,
-      reportDate: data?.reportDate || "N/A"
-    }
-  ];
-
-  const renderDSMGrid = (
-    data: VestedAmountsByAge,
-    columns: ReturnType<typeof GetVestedAmountsByAgeColumns>,
-    key: string
-  ) => {
-    const summaryRows = createSummaryRows(data);
-
-    return (
-      <DSMGrid
-        preferenceKey={key}
-        isLoading={isLoading}
-        handleSortChanged={sortEventHandler}
-        providedOptions={{
-          rowData: [...summaryRows, ...(data.response?.results || [])],
-          pinnedTopRowData: summaryRows,
-          columnDefs: [
-            ...columns,
-            {
-              headerName: "Summary",
-              children: [
-                { headerName: "Report Name", field: "reportName" },
-                { headerName: "Report Date", field: "reportDate" },
-                { headerName: "FT 100% Vested Amount", field: "totalFullTime100PercentAmount" },
-                { headerName: "FT Partial Vested Amount", field: "totalFullTimePartialAmount" },
-                { headerName: "FT Not Vested Amount", field: "totalFullTimeNotVestedAmount" },
-                { headerName: "PT 100% Vested Amount", field: "totalPartTime100PercentAmount" },
-                { headerName: "PT Partial Vested Amount", field: "totalPartTimePartialAmount" },
-                { headerName: "PT Not Vested Amount", field: "totalPartTimeNotVestedAmount" },
-                { headerName: "Beneficiary Count", field: "totalBeneficiaryCount" },
-                { headerName: "Beneficiary Amount", field: "totalBeneficiaryAmount" },
-                { headerName: "FT Total Count", field: "totalFullTimeCount" },
-                { headerName: "Not Vested Count", field: "totalNotVestedCount" },
-                { headerName: "Partial Vested Count", field: "totalPartialVestedCount" }
-              ]
-            }
-          ]
-        }}
-      />
-    );
-  };
+  const columnDefs = GetVestedAmountsByAgeColumns(countColName, amountColName);
 
   return (
     <>
@@ -88,14 +40,18 @@ const VestedAmountsByAgeGrid = () => {
             <Typography
               variant="h2"
               sx={{ color: "#0258A5" }}>
-              {vestedAmountsByAge.reportName}
+              {`${gridTitle} ${totalCount != undefined ? `(${vestedAmountsByAge[totalCount]})` : ""}`}
             </Typography>
           </div>
-          <Grid2
-            container
-            spacing={2}>
-            <Grid2 xs={12}>{renderDSMGrid(vestedAmountsByAge, columnDefsTotal, "Vesting Amounts by Age")}</Grid2>
-          </Grid2>
+          <DSMGrid
+            preferenceKey={gridTitle}
+            isLoading={isLoading}
+            handleSortChanged={sortEventHandler}
+            providedOptions={{
+              rowData: vestedAmountsByAge?.response.results,
+              columnDefs: columnDefs
+            }}
+          />
         </>
       )}
     </>
