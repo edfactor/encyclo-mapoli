989060d4f39f449f58f446dae692b8306a508bed
Ed Lyons <ed.lyons@ryan.com>
2025-03-03 11:19:43 -0500

    Fix object property

--- src/ui/src/pages/NegativeEtvaForSSNsOnPayprofit/NegativeEtvaForSSNsOnPayprofitGrid.tsx
+++ src/ui/src/pages/NegativeEtvaForSSNsOnPayprofit/NegativeEtvaForSSNsOnPayprofitGrid.tsx
@@ -1,12 +1,12 @@
-import { Link, Typography } from "@mui/material";
-import { useState, useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { Typography } from "@mui/material";
+import { useState, useMemo, useCallback } from "react";
+import { useSelector } from "react-redux";
 import { useLazyGetNegativeEVTASSNQuery } from "reduxstore/api/YearsEndApi";
 import { RootState } from "reduxstore/store";
 import { DSMGrid, ISortParams, Pagination } from "smart-ui-library";
 import { GetNegativeEtvaForSSNsOnPayProfitColumns } from "./NegativeEtvaForSSNsOnPayprofitGridColumn";
 import { NegativeEtvaForSSNsOnPayProfit } from "reduxstore/types";
-import { ICellRendererParams } from "ag-grid-community";
+import { Path, useNavigate } from "react-router";
 
 const NegativeEtvaForSSNsOnPayprofitGrid = () => {
   const [pageNumber, setPageNumber] = useState(0);
@@ -15,65 +15,76 @@ const NegativeEtvaForSSNsOnPayprofitGrid = () => {
     sortBy: "Badge",
     isSortDescending: false
   });
-  
-  const dispatch = useDispatch();
+
   const { negativeEtvaForSSNsOnPayprofit } = useSelector((state: RootState) => state.yearsEnd);
   const [_, { isLoading }] = useLazyGetNegativeEVTASSNQuery();
 
   const sortEventHandler = (update: ISortParams) => setSortParams(update);
-  const columnDefs = useMemo(() => GetNegativeEtvaForSSNsOnPayProfitColumns(viewBadge), []);
 
+  const navigate = useNavigate();
+  // Wrapper to pass react function to non-react class
+  const handleNavigationForButton = useCallback(
+    (destination: string | Partial<Path>) => {
+      navigate(destination);
+    },
+    [navigate]
+  );
+
+  const columnDefs = useMemo(
+    () => GetNegativeEtvaForSSNsOnPayProfitColumns(handleNavigationForButton),
+    [handleNavigationForButton]
+  );
 
   const dummyETVAData: NegativeEtvaForSSNsOnPayProfit[] = [
     {
       employeeBadge: 700127,
-      employeeSsn: 123456789,
+      ssn: 123456789,
       etvaValue: -1234.56
     },
     {
       employeeBadge: 234567,
-      employeeSsn: 234567890,
-      etvaValue: -42.10
+      ssn: 234567890,
+      etvaValue: -42.1
     },
     {
       employeeBadge: 345678,
-      employeeSsn: 345678901,
+      ssn: 345678901,
       etvaValue: -999.99
     },
     {
       employeeBadge: 456789,
-      employeeSsn: 456789012,
-      etvaValue: -5000.00
+      ssn: 456789012,
+      etvaValue: -5000.0
     },
     {
       employeeBadge: 567890,
-      employeeSsn: 567890123,
-      etvaValue: -1.50
+      ssn: 567890123,
+      etvaValue: -1.5
     },
     {
       employeeBadge: 678901,
-      employeeSsn: 678901234,
+      ssn: 678901234,
       etvaValue: -750.25
     },
     {
       employeeBadge: 789012,
-      employeeSsn: 789012345,
+      ssn: 789012345,
       etvaValue: -3333.33
     },
     {
       employeeBadge: 890123,
-      employeeSsn: 890123456,
+      ssn: 890123456,
       etvaValue: -15.75
     },
     {
       employeeBadge: 901234,
-      employeeSsn: 901234567,
-      etvaValue: -2500.00
+      ssn: 901234567,
+      etvaValue: -2500.0
     },
     {
       employeeBadge: 12345,
-      employeeSsn: 12345678,
-      etvaValue: -175.80
+      ssn: 12345678,
+      etvaValue: -175.8
     }
   ];
 
