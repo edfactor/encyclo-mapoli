53234921f09323ff8ec68bb64fb131ec81863231
Phil Denoncourt <pdenoncourt@demoulasmarketbaseket.com>
2025-03-11 08:13:36 -0400

    Add supports for totals on the profit sharing report

--- src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/YearEnd/YearEndProfitSharingReportResponse.cs
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/YearEnd/YearEndProfitSharingReportResponse.cs
@@ -1,5 +1,42 @@
-﻿namespace Demoulas.ProfitSharing.Common.Contracts.Response.YearEnd;
-public sealed record YearEndProfitSharingReportResponse
+﻿using Demoulas.Common.Contracts.Contracts.Request;
+using Demoulas.Common.Contracts.Contracts.Response;
+
+namespace Demoulas.ProfitSharing.Common.Contracts.Response.YearEnd;
+public sealed record YearEndProfitSharingReportResponse : ReportResponseBase<YearEndProfitSharingReportDetail>
+{
+    public decimal WagesTotal { get; set; }
+    public decimal HoursTotal { get; set; }
+    public decimal PointsTotal { get; set; }
+    public decimal TerminatedWagesTotal { get; set; }
+    public decimal TerminatedHoursTotal { get; set; }
+    public int NumberOfEmployees { get; set; }
+    public int NumberOfNewEmployees { get; set; }
+    public int NumberOfEmployeesUnder21 { get; set; }
+    public int NumberOfEmployeesInPlan { get; set; }
+
+    public static YearEndProfitSharingReportResponse ResponseExample()
+    {
+        return new YearEndProfitSharingReportResponse()
+        {
+            ReportName = "yearend-profit-sharing-report",
+            ReportDate = DateTime.UtcNow,
+            WagesTotal = 291941.55m,
+            HoursTotal = 10052,
+            PointsTotal = 2919m,
+            TerminatedWagesTotal = 2002.51m,
+            TerminatedHoursTotal = 205,
+            NumberOfEmployees = 2051,
+            NumberOfNewEmployees = 55,
+            NumberOfEmployeesInPlan = 2044,
+            NumberOfEmployeesUnder21 = 015,
+            Response = new PaginatedResponseDto<YearEndProfitSharingReportDetail>(new PaginationRequestDto())
+                { Total = 1, Results = new List<YearEndProfitSharingReportDetail>() { YearEndProfitSharingReportDetail.ResponseExample() } }
+                
+        };
+    }
+}
+
+public sealed record YearEndProfitSharingReportDetail
 {
     public required int BadgeNumber { get; set; }
     public required string EmployeeName { get; set; }
@@ -18,9 +55,9 @@ public sealed record YearEndProfitSharingReportResponse
     public required decimal Balance { get; set; }
     public required short YearsInPlan { get; set; }
 
-    public static YearEndProfitSharingReportResponse ResponseExample()
+    public static YearEndProfitSharingReportDetail ResponseExample()
     {
-        return new YearEndProfitSharingReportResponse()
+        return new YearEndProfitSharingReportDetail()
         {
             BadgeNumber = 135,
             EmployeeName = "John Doe",
@@ -41,3 +78,4 @@ public sealed record YearEndProfitSharingReportResponse
         };
     }
 }
+
