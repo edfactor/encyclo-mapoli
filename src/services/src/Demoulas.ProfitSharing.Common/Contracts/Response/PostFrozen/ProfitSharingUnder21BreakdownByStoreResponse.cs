504553397355fe7ec7d1468cc686b151307d8319
Phil Denoncourt <pdenoncourt@demoulasmarketbaseket.com>
2025-03-13 08:27:29 -0400

    Initial work on PS-759 - First endpoint

---
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/PostFrozen/ProfitSharingUnder21BreakdownByStoreResponse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demoulas.ProfitSharing.Common.Contracts.Response.PostFrozen
+{
+    public sealed record ProfitSharingUnder21BreakdownByStoreResponse
+    {
+        public short StoreNumber { get; set; }
+        public int BadgeNumber { get; set; }
+        public required string FullName { get; set; }
+        public decimal? BeginningBalance { get; set; } = 0;
+        public decimal? Earnings { get; set; } = 0;
+        public decimal? Contributions { get; set; } = 0;
+        public decimal? Forfeitures { get; set; } = 0;
+        public decimal? Distributions { get; set; } = 0;
+        public decimal? EndingBalance { get; set; } = 0;
+        public decimal? VestedAmount { get; set; } = 0;
+        public decimal? VestingPercentage { get; set; } = 0;
+        public DateOnly DateOfBirth { get; set; }
+        public byte Age { get; set; }
+        public byte EnrollmentId { get; set; }
+
+        public static readonly string REPORT_NAME = "PROFIT SHARING UNDER 21 BREAKDOWN REPORT";
+        public static ProfitSharingUnder21BreakdownByStoreResponse ResponseExample()
+        {
+            return new ProfitSharingUnder21BreakdownByStoreResponse()
+            {
+                StoreNumber = (short)42,
+                BadgeNumber = 704234,
+                FullName = "Johnson, Martha",
+                BeginningBalance = 20014,
+                Earnings = 199.25m,
+                Contributions = 59.11m,
+                Forfeitures = 0,
+                Distributions = 0,
+                VestedAmount = 20272.36m,
+                EndingBalance = 20272.36m,
+                VestingPercentage = 1,
+                DateOfBirth = new DateOnly(1971,5,11),
+                Age = 53,
+                EnrollmentId = 1
+            };
+        }
+    }
+}
