0f80188d80cc5bb2d5706a404c2390bc868cb70a
Phil Denoncourt <pdenoncourt@demoulasmarketbaseket.com>
2025-03-18 15:22:41 -0400

    Initial implementation of inactive members list

---
+++ src/services/tests/Demoulas.ProfitSharing.UnitTests/Reports/PostFrozen/ProfitSharingUnder21InactiveNoBalanceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Demoulas.Common.Contracts.Contracts.Response;
+using Demoulas.ProfitSharing.Api;
+using Demoulas.ProfitSharing.Common.Contracts.Request;
+using Demoulas.ProfitSharing.Common.Contracts.Response.PostFrozen;
+using Demoulas.ProfitSharing.Endpoints.Endpoints.Reports.YearEnd.PostFrozen;
+using Demoulas.ProfitSharing.Security;
+using Demoulas.ProfitSharing.UnitTests.Common.Base;
+using Demoulas.ProfitSharing.UnitTests.Common.Extensions;
+using FastEndpoints;
+using FluentAssertions;
+
+namespace Demoulas.ProfitSharing.UnitTests.Reports.PostFrozen
+{
+    public class ProfitSharingUnder21InactiveNoBalanceTests : ApiTestBase<Program>
+    {
+        [Fact(DisplayName = "PS-759 - Profit Sharing under 21 inactive/nobalance")]
+        public async Task CheckUnder21InactiveNoBalanceTests()
+        {
+            var request = new ProfitYearRequest() { ProfitYear = 2024, Skip = 0, Take = 255 };
+            var response = await ApiClient.GETAsync<ProfitSharingUnder21InactiveNoBalanceEndpoint, ProfitYearRequest, PaginatedResponseDto<ProfitSharingUnder21InactiveNoBalanceResponse>>(request);
+
+            response.Should().NotBeNull();
+            response.Response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+            ApiClient.CreateAndAssignTokenForClient(Role.FINANCEMANAGER);
+            
+            response = await ApiClient.GETAsync<ProfitSharingUnder21InactiveNoBalanceEndpoint, ProfitYearRequest, PaginatedResponseDto<ProfitSharingUnder21InactiveNoBalanceResponse>>(request);
+            response.Should().NotBeNull();
+            response.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        }
+    }
+}
