7e05ebd20e79b5163a9e38ff06fde39cd26173c9
Phil Denoncourt <pdenoncourt@demoulasmarketbaseket.com>
2025-03-19 08:56:49 -0400

    Initial implementation of the totals needed for under 21 report

---
+++ src/services/tests/Demoulas.ProfitSharing.UnitTests/Reports/PostFrozen/ProfitSharingUnder21TotalsTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using Demoulas.ProfitSharing.Api;
+using Demoulas.ProfitSharing.Common.Contracts.Request;
+using Demoulas.ProfitSharing.Common.Contracts.Response.PostFrozen;
+using Demoulas.ProfitSharing.Endpoints.Endpoints.Reports.YearEnd.PostFrozen;
+using Demoulas.ProfitSharing.Security;
+using Demoulas.ProfitSharing.UnitTests.Common.Base;
+using Demoulas.ProfitSharing.UnitTests.Common.Extensions;
+using FastEndpoints;
+using FluentAssertions;
+
+namespace Demoulas.ProfitSharing.UnitTests.Reports.PostFrozen
+{
+    public sealed class ProfitSharingUnder21TotalsTests:ApiTestBase<Program>
+    {
+        [Fact(DisplayName = "PS-759 - Profit Sharing under 21 Totals")]
+        public async Task CheckUnder21Totals()
+        {
+            var request = new ProfitYearRequest() { ProfitYear = 2024, Skip = 0, Take = 255 };
+            var response = await ApiClient.GETAsync<ProfitSharingUnder21TotalsEndpoint, ProfitYearRequest, ProfitSharingUnder21TotalsResponse>(request);
+
+            response.Should().NotBeNull();
+            response.Response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+            ApiClient.CreateAndAssignTokenForClient(Role.FINANCEMANAGER);
+
+            response = await ApiClient.GETAsync<ProfitSharingUnder21TotalsEndpoint, ProfitYearRequest, ProfitSharingUnder21TotalsResponse>(request);
+            response.Should().NotBeNull();
+            response.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+    }
+}
