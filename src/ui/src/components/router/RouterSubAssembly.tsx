33cc106c1d52b8592f121f0c540ad474529819f4
Russ Harding <rharding@mainoffice.demoulas.corp>
2025-03-25 11:09:14 -0400

    Update rehire forfeitures component in RouterSubAssembly
    Replaced `MilitaryAndRehireForfeitures` with `RehireForfeitures` in imports and routing configuration. This change shifts the focus of the route to handle rehire forfeitures specifically.

--- src/ui/src/components/router/RouterSubAssembly.tsx
+++ src/ui/src/components/router/RouterSubAssembly.tsx
@@ -18,7 +18,7 @@ import ProfitShareReportFinalRun from "pages/FiscalFlow/ProfitShareReportFinalRu
 import Forfeit from "pages/Forfeit/Forfeit";
 import FrozenSummary from "pages/FrozenSummary/FrozenSummary";
 import MasterInquiry from "pages/MasterInquiry/MasterInquiry";
-import MilitaryAndRehireForfeitures from "pages/MilitaryAndRehireForfeitures/MilitaryAndRehireForfeitures";
+import RehireForfeitures from "pages/RehireForfeitures/RehireForfeitures";
 import MilitaryEntryAndModification from "pages/MilitaryEntryAndModification/MilitaryEntryAndModification";
 import EighteenToTwenty from "pages/PAY426Reports/PAY426-1/EighteenToTwenty";
 import Beneficiaries from "pages/PAY426Reports/PAY426-10/Beneficiaries";
@@ -179,7 +179,7 @@ const RouterSubAssembly: React.FC = () => {
                 element={<EmployeesOnMilitaryLeave />}></Route>
               <Route
                 path={ROUTES.REHIRE_FORFEITURES}
-                element={<MilitaryAndRehireForfeitures />}></Route>
+                element={<RehireForfeitures />}></Route>
               <Route
                 path={ROUTES.DISTRIBUTIONS_AND_FORFEITURES}
                 element={<DistributionsAndForfeitures />}></Route>
