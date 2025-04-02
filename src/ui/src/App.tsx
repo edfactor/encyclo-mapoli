8825be5a67ae2ee59c801fbdc92c660e545027b2
Dakota Kim <codingvampire@gmail.com>
2025-04-02 09:57:32 -0400

    use buildInfo generated by build pipeline OR local dev

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -4,8 +4,20 @@ import PSLayout from "components/Layout/PSLayout";
 import { themeOptions } from "smart-ui-library";
 import "smart-ui-library/dist/smart-ui-library.css";
 import "../agGridConfig";
-import buildInfo from "./.buildinfo.json";
 import Router from "./components/router/Router";
+
+let buildInfo: { BuildNumber: string; BuildId: string; Branch: string; CommitHash: string };
+
+try {
+  buildInfo = require("./.buildinfo.json");
+} catch (e) {
+  buildInfo = {
+    BuildNumber: "Local",
+    BuildId: "Dev",
+    Branch: "Local Development",
+    CommitHash: ""
+  };
+}
 
 const App = () => {
   const onClick = (_e: React.MouseEvent<HTMLDivElement>) => {};
