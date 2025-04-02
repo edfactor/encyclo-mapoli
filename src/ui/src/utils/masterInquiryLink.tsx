9ddbca7e2bfac505e4101ed69f7954c0ed9bd8f1
Russ Harding <rharding@mainoffice.demoulas.corp>
2025-04-02 15:13:08 -0400

    Enhance badge link rendering with suffix support
    Updated `GetMasterInquiryGridColumns` to pass `psnSuffix` to `viewBadgeLinkRenderer`.
    Modified `viewBadgeLinkRenderer` to accept an optional `psnSuffix`, defaulting to 0 if undefined.
    Adjusted logic to construct `safeValue` and `displayValue` based on the presence of `psnSuffix`.
    Updated link and button rendering to display the correct badge number and suffix, and changed the maximum badge number condition to 800,000.

--- src/ui/src/utils/masterInquiryLink.tsx
+++ src/ui/src/utils/masterInquiryLink.tsx
@@ -2,25 +2,29 @@ import { Button } from "@mui/material";
 import Link from "@mui/material/Link";
 
 // Some callers want a MUI Link, some want a Button with a react navigate function
-export const viewBadgeLinkRenderer = (badgeNumber: number, navigateFunction?: (path: string) => void) => {
-  if (!badgeNumber || badgeNumber > 8000000000) return badgeNumber;
-  const safeValue = badgeNumber.toString(); // Ens
+export const viewBadgeLinkRenderer = (badgeNumber: number, psnSuffix?: number, 
+                                      navigateFunction?: (path: string) => void) => {
+  if (!badgeNumber || badgeNumber > 800000) return badgeNumber;
+  if (psnSuffix === undefined) psnSuffix = 0;
+  
+  const safeValue = psnSuffix > 0 ? badgeNumber.toString() + psnSuffix.toString() : badgeNumber.toString();
+  const displayValue = psnSuffix > 0 ? `${badgeNumber}-${psnSuffix}` : badgeNumber;
 
   if (navigateFunction === undefined) {
     return (
       <Link
         className="h-5 solid underline normal-case"
-        href={`/master-inquiry/${safeValue.slice(0, 6)}`}>
-        {safeValue}
+        href={`/master-inquiry/${safeValue}`}>
+        {displayValue}
       </Link>
     );
   } else {
     return (
       <Button
-        href={`/master-inquiry/${safeValue.slice(0, 6)}`}
+        href={`/master-inquiry/${safeValue}`}
         variant="text"
-        onClick={() => navigateFunction(`/master-inquiry/${safeValue.slice(0, 6)}`)}>
-        {safeValue}
+        onClick={() => navigateFunction(`/master-inquiry/${safeValue}`)}>
+        {displayValue}
       </Button>
     );
   }
