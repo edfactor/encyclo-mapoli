02ba0482206e06407bd9e559bebac449697b1f60
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2025-04-06 21:11:24 -0400

    formatting

--- src/services/src/Demoulas.ProfitSharing.Services/Internal/ProfitShareUpdate/BeneficiaryFinancials.cs
+++ src/services/src/Demoulas.ProfitSharing.Services/Internal/ProfitShareUpdate/BeneficiaryFinancials.cs
@@ -5,9 +5,8 @@
 /// </summary>
 internal sealed record BeneficiaryFinancials
 {
-     public required string? Psn { get; set; } // PIC 9(11).
-     public required int Ssn { get; set; } // PIC 9(9).
+    public required string? Psn { get; set; } // PIC 9(11).
+    public required int Ssn { get; set; } // PIC 9(9).
     public required string? Name { get; set; } // PIC X(25).
-    
     public required decimal BeginningBalance { get; set; } // PIC S9(7)V99.
 }
