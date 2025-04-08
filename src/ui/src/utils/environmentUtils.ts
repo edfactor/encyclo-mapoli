85cb80bff6c1830ad94984112fff1b31f96386c6
Russ Harding <rharding@mainoffice.demoulas.corp>
2025-04-08 13:31:12 -0400

    Add EnvironmentUtils class for environment management
    Introduces the `EnvironmentUtils` class with static getter methods to determine the application's environment (development, qa, uat, production). Includes checks for Okta enablement and a method for the post-logout redirect URI.

---
+++ src/ui/src/utils/environmentUtils.ts
@@ -0,0 +1,33 @@
+export default class EnvironmentUtils {
+  public static get isDevelopment(): boolean {
+    return import.meta.env.VITE_APP_ENVIRONMENT === "development";
+  }
+
+  public static get postLogoutRedirectUri(): string {
+    return "https://marketbasket.okta.com/login/default";
+  }
+
+  public static get isQA(): boolean {
+    return import.meta.env.VITE_APP_ENVIRONMENT === "qa";
+  }
+
+  public static get isDevelopmentOrQA(): boolean {
+    return this.isDevelopment || this.isQA;
+  }
+
+  public static get isUAT(): boolean {
+    return import.meta.env.VITE_APP_ENVIRONMENT === "uat";
+  }
+
+  public static get isProduction(): boolean {
+    return import.meta.env.VITE_APP_ENVIRONMENT === "production";
+  }
+
+  public static get isOktaEnabled(): boolean {
+    return import.meta.env.VITE_APP_OKTA_ENABLED === "true";
+  }
+
+  public static get envMode(): "development" | "qa" | "uat" | "production" {
+    return import.meta.env.VITE_APP_ENVIRONMENT;
+  }
+}
