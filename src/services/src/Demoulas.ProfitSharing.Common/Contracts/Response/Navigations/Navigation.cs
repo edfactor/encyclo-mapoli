c7e90287b2ee083c9b470c7ce526ab813d5fd1e8
HassamSherazi <HassamSherazi@HASSAM>
2025-04-09 12:53:49 -0400

    Created API to get navigation - Added Navigation Dtos in common library - Added hardcoded values of navigation in the constructor of NavigationService - Tested and working.

---
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/Navigations/Navigation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demoulas.ProfitSharing.Common.Contracts.Response.Navigations;
+public record Navigation
+{
+    public int Id { get; set; }
+    public int ParentId { get; set; }
+    public required string Title { get; set; }
+    public string? SubTitle { get; set; }
+    public required string Url { get; set; }
+    public byte? Status { get; set; }
+    public byte OrderNumber { get; set; }
+    public string? Icon { get; set; }
+    public List<string>? RequiredRoles { get; set; }
+    public List<Navigation>? Children { get; set; }
+}
---
+++ src/services/src/Demoulas.ProfitSharing.Endpoints/Endpoints/Navigations/GetNavigation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demoulas.ProfitSharing.Common.Contracts.Request.Naviations;
+using Demoulas.ProfitSharing.Common.Contracts.Response.Navigations;
+using Demoulas.ProfitSharing.Common.Interfaces.Navigations;
+using Demoulas.ProfitSharing.Endpoints.Groups;
+using FastEndpoints;
+
+namespace Demoulas.ProfitSharing.Endpoints.Endpoints.Navigations;
+public class GetNavigation: Endpoint<NavigationRequestDto,NavigationResponseDto>
+{
+
+    private readonly INavigationService _navigationService;
+
+    public GetNavigation(INavigationService navigationService)
+    {
+        _navigationService = navigationService;
+    }
+
+    public override void Configure()
+    {
+        AllowAnonymous();
+        Get($"/navigation/list");
+        Summary(m =>
+        {
+            m.Summary = "Get all navigation";
+            m.Description = "Fetch List of navigation object.";
+            m.ResponseExamples = new Dictionary<int, object> { { 200, new NavigationResponseDto() } };
+        });
+        Group<NavigationGroup>();
+    }
+
+    public override Task<NavigationResponseDto> ExecuteAsync(NavigationRequestDto req, CancellationToken ct)
+    {
+        var navigationList = this._navigationService.GetNavigation();
+        var response = new NavigationResponseDto { Navigation = navigationList };
+        return Task.FromResult(response);
+    }
+
+}
