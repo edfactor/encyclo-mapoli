a1174cd1d08121169d81062fb62a310f190c597f
Phil Denoncourt <pdenoncourt@demoulasmarketbasket.com>
2025-04-09 15:23:20 +0000

    Merged in feature/PS-762 (pull request #444)

---
+++ src/services/tests/Demoulas.ProfitSharing.UnitTests/Reports/PostFrozen/NewProfitSharingLabelsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demoulas.Common.Contracts.Contracts.Response;
+using Demoulas.ProfitSharing.Api;
+using Demoulas.ProfitSharing.Common.Contracts.Request;
+using Demoulas.ProfitSharing.Common.Contracts.Response;
+using Demoulas.ProfitSharing.Common.Contracts.Response.PostFrozen;
+using Demoulas.ProfitSharing.Endpoints.Endpoints.Reports.YearEnd.PostFrozen;
+using Demoulas.ProfitSharing.Security;
+using Demoulas.ProfitSharing.UnitTests.Common.Base;
+using Demoulas.ProfitSharing.UnitTests.Common.Extensions;
+using FastEndpoints;
+using FluentAssertions;
+using MassTransit;
+
+namespace Demoulas.ProfitSharing.UnitTests.Reports.PostFrozen;
+public class NewProfitSharingLabelsTests : ApiTestBase<Program>
+{
+    [Fact(DisplayName = "PS-762 - New Profit Sharing labels")]
+    public async Task CheckNewProfitSharingLabels()
+    {
+        var request = new ProfitYearRequest() { ProfitYear = 2024 };
+        var response = await ApiClient.GETAsync<NewProfitSharingLabelsEndpoint, ProfitYearRequest, PaginatedResponseDto<NewProfitSharingLabelResponse>>(request);
+        response.Should().NotBeNull();
+        response.Response.StatusCode.Should().Be(System.Net.HttpStatusCode.Unauthorized);
+
+        ApiClient.CreateAndAssignTokenForClient(Role.FINANCEMANAGER);
+
+        response = await ApiClient.GETAsync<NewProfitSharingLabelsEndpoint, ProfitYearRequest, PaginatedResponseDto<NewProfitSharingLabelResponse>>(request);
+        response.Should().NotBeNull();
+        response.Response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+    }
+}
