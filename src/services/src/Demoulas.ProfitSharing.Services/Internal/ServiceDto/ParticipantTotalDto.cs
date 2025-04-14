765e842c980193b4dfd3587682bdba0a757abb5a
Phil Denoncourt <pdenoncourt@demoulasmarketbaseket.com>
2025-04-14 11:33:02 -0400

    Cleanup

--- src/services/src/Demoulas.ProfitSharing.Services/Internal/ServiceDto/ParticipantTotalDto.cs
+++ src/services/src/Demoulas.ProfitSharing.Services/Internal/ServiceDto/ParticipantTotalDto.cs
@@ -1,10 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
+﻿
 namespace Demoulas.ProfitSharing.Services.Internal.ServiceDto;
-public sealed class ParticipantTotalDto
+internal sealed class ParticipantTotalDto
 {
-    [Column("SSN")]
     internal int Ssn { get; set; }
-    [Column("TOTAL")]
     internal decimal? Total { get; set; }
 }
