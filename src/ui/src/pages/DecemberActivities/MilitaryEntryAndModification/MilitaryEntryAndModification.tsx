f0dc3a943a66908b08d320d61cc080291c3b67d7
Russ Harding <rharding@demoulasmarketbasket.com>
2025-04-15 15:44:00 -0400

    Refactor military entry components and update imports
    - Updated import statements in `MilitaryEntryAndModification.tsx` to include `ISortParams` and corrected the import for `useLazyGetMilitaryContributionsQuery`.
    - Replaced `MilitaryAndRehireEntryAndModificationSearchFilter` with `MilitaryEntryAndModificationSearchFilter`.
    - Modified `handleFetchContributions` to include `contributionAmount` and `contributionDate` in the fetch parameters.
    - Changed sorting criteria in the `onSubmit` function of `MilitaryEntryAndModificationSearchFilter.tsx` from `profitYear` to `badgeNumber`.

--- src/ui/src/pages/DecemberActivities/MilitaryEntryAndModification/MilitaryEntryAndModification.tsx
+++ src/ui/src/pages/DecemberActivities/MilitaryEntryAndModification/MilitaryEntryAndModification.tsx
@@ -2,24 +2,20 @@ import { Dialog, DialogContent, DialogTitle, Divider } from "@mui/material";
 import Grid2 from "@mui/material/Grid2";
 import StatusDropdownActionNode from "components/StatusDropdownActionNode";
 import { useCallback, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "reduxstore/store";
-import { DSMAccordion, Page } from "smart-ui-library";
+import { DSMAccordion, ISortParams, Page } from "smart-ui-library";
 import { CAPTIONS } from "../../../constants";
-import { useLazyGetMilitaryContributionsQuery } from "../../../reduxstore/api/MilitaryApi";
 import MilitaryContributionForm from "./MilitaryContributionForm";
 import MilitaryContributionGrid from "./MilitaryContributionFormGrid";
 import { RootState } from "reduxstore/store";
-import {
-  useLazyGetMilitaryContributionsQuery
-} from "../../../reduxstore/api/MilitaryApi";
+import { useLazyGetMilitaryContributionsQuery} from "../../../reduxstore/api/MilitaryApi";
 import { useSelector } from "react-redux";
 import useDecemberFlowProfitYear from "../../../hooks/useDecemberFlowProfitYear";
+import MilitaryEntryAndModificationSearchFilter from "./MilitaryEntryAndModificationSearchFilter";
 
 const MilitaryEntryAndModification = () => {
   const [initialSearchLoaded, setInitialSearchLoaded] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [showContributions, setShowContributions] = useState(false);
+  const [showContributions, setShowContributions] = useState(false);  
   const { masterInquiryEmployeeDetails } = useSelector((state: RootState) => state.inquiry);
   const [fetchContributions, { isFetching }] = useLazyGetMilitaryContributionsQuery();
   const profitYear = useDecemberFlowProfitYear();
@@ -43,8 +39,13 @@ const MilitaryEntryAndModification = () => {
       fetchContributions({
         badgeNumber: Number(masterInquiryEmployeeDetails.badgeNumber),
         profitYear: profitYear,
-        pagination: { skip: 0, take: 25, sortBy: "contributionDate", isSortDescending: false }
-      });
+        contributionAmount: 0,
+        contributionDate: "",
+        pagination: {
+          skip: 0,
+          take: 25,
+          sortBy: "contributionDate",
+          isSortDescending: false}});
       setShowContributions(true);
     }
   }, [masterInquiryEmployeeDetails, fetchContributions]);
@@ -67,7 +68,7 @@ const MilitaryEntryAndModification = () => {
         </Grid2>
         <Grid2 width={"100%"}>
           <DSMAccordion title="Filter">
-            <MilitaryAndRehireEntryAndModificationSearchFilter setInitialSearchLoaded={setInitialSearchLoaded} />
+            <MilitaryEntryAndModificationSearchFilter setInitialSearchLoaded={setInitialSearchLoaded} />
           </DSMAccordion>
         </Grid2>
 
