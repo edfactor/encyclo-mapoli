d5f433586cc1cc5ab8fa1d4eae6415c341896ca2
Russ Harding <rharding@demoulasmarketbasket.com>
2025-04-16 19:55:40 -0400

    Add some Okta Details

--- src/services/src/Demoulas.ProfitSharing.Endpoints/HealthCheck/EnvironmentHealthCheck.cs
+++ src/services/src/Demoulas.ProfitSharing.Endpoints/HealthCheck/EnvironmentHealthCheck.cs
@@ -1,4 +1,5 @@
 ﻿using Demoulas.Common.Api.Utilities;
+using Demoulas.Common.Contracts.Configuration;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace Demoulas.ProfitSharing.Endpoints.HealthCheck;
@@ -9,12 +10,16 @@ public class EnvironmentHealthCheck : IHealthCheck
 {
     private readonly IWebHostEnvironment _env;
     private readonly AppVersionInfo _appVersion;
+    private readonly OktaConfiguration _oktaConfiguration;
     private static readonly DateTime _startupTime = DateTime.UtcNow;
 
-    public EnvironmentHealthCheck(IWebHostEnvironment env, AppVersionInfo appVersion)
+    public EnvironmentHealthCheck(IWebHostEnvironment env, 
+        AppVersionInfo appVersion,
+        OktaConfiguration oktaConfiguration)
     {
         _env = env;
         _appVersion = appVersion;
+        _oktaConfiguration = oktaConfiguration;
     }
 
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
@@ -29,7 +34,11 @@ public class EnvironmentHealthCheck : IHealthCheck
             { "AppVersion", _appVersion.BuildNumber },
             { "CurrentDirectory", Environment.CurrentDirectory },
             { "Uptime", (DateTime.UtcNow - _startupTime).ToString(@"dd\.hh\:mm\:ss") },
-            { "UtcNow", DateTimeOffset.UtcNow.ToString("o") }
+            { "UtcNow", DateTimeOffset.UtcNow.ToString("o") },
+            { "OktaEnvironmentName", _oktaConfiguration.EnvironmentName ?? string.Empty },
+            { "OktaRolePrefix", _oktaConfiguration.RolePrefix },
+            { "OktaAuthorizationServerId", _oktaConfiguration.AuthorizationServerId },
+            { "OktaAudience", _oktaConfiguration.Audience }
         };
 
         return Task.FromResult(HealthCheckResult.Healthy("Environment check", data));
