b882fa4b3689ed70b4aadb0af474194dae89f5a2
Dakota Kim <codingvampire@gmail.com>
2025-04-17 17:58:47 -0400

    no dispatch needed

--- src/ui/src/components/MenuBar/OktaLoginCallback.tsx
+++ src/ui/src/components/MenuBar/OktaLoginCallback.tsx
@@ -1,8 +1,6 @@
 import { LoginCallback } from "@okta/okta-react";
-import { useDispatch } from "react-redux";
 
 const OktaLoginCallback = () => {
-  const dispatch = useDispatch();
   const params = new URLSearchParams(document.location.search);
   const errorCode = params.get("error");
   const errorDescription = params.get("error_description");
@@ -13,7 +11,6 @@ const OktaLoginCallback = () => {
         <h2>Access Denied</h2>
         <p>You are not assigned to this application in Okta.</p>
         <p>Please contact your administrator to request access to this environment.</p>
-        <a href="/">Return to login</a>
       </div>
     );
   }
@@ -23,7 +20,6 @@ const OktaLoginCallback = () => {
       <div style={{ padding: "20px", textAlign: "center" }}>
         <h2>Authentication Error</h2>
         <p>{errorDescription || errorCode}</p>
-        <a href="/">Try again</a>
       </div>
     );
   }
