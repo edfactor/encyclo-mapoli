14d7a4931c3356e1ef6b0cd0027e30d22dbc21a8
Dakota Kim <codingvampire@gmail.com>
2025-04-21 17:16:15 -0400

    placeholder employee details

--- src/ui/src/pages/ForfeituresAdjustment/ForfeituresAdjustment.tsx
+++ src/ui/src/pages/ForfeituresAdjustment/ForfeituresAdjustment.tsx
@@ -6,14 +6,51 @@ import { CAPTIONS } from "../../constants";
 import ForfeituresAdjustmentGrid from "./ForfeituresAdjustmentGrid";
 import ForfeituresAdjustmentSearchParameters from "./ForfeituresAdjustmentSearchParameters";
 import StatusDropdownActionNode from "components/StatusDropdownActionNode";
+import MasterInquiryEmployeeDetails from "pages/MasterInquiry/MasterInquiryEmployeeDetails";
+import { EmployeeDetails } from "reduxstore/types";
 
 const ForfeituresAdjustment = () => {
   const [initialSearchLoaded, setInitialSearchLoaded] = useState(false);
+  const [showEmployeeDetails, setShowEmployeeDetails] = useState(false);
+
+  // Dummy data for employee details - will be replaced with real stuff later
+  const dummyEmployeeDetails: EmployeeDetails = {
+    firstName: "First",
+    lastName: "LastName",
+    address: "123 Main Street",
+    addressCity: "City Name",
+    addressState: "MA",
+    addressZipCode: "02134",
+    dateOfBirth: "1990-01-01",
+    ssn: "***-**-1234",
+    yearToDateProfitSharingHours: 0,
+    yearsInPlan: 0,
+    percentageVested: 0,
+    contributionsLastYear: false,
+    enrollmentId: 1,
+    enrollment: "Yes",
+    badgeNumber: "12345",
+    hireDate: "2018-01-01",
+    terminationDate: null,
+    reHireDate: null,
+    storeNumber: 0,
+    beginPSAmount: 0,
+    currentPSAmount: 0,
+    beginVestedAmount: 0,
+    currentVestedAmount: 0,
+    currentEtva: 0,
+    previousEtva: 0
+  };
 
   const renderActionNode = () => {
     return (
       <StatusDropdownActionNode />
     );
+  };
+
+  const handleSearchComplete = (loaded: boolean) => {
+    setInitialSearchLoaded(loaded);
+    setShowEmployeeDetails(loaded);
   };
 
   return (
@@ -26,9 +63,11 @@ const ForfeituresAdjustment = () => {
         </Grid2>
         <Grid2 width={"100%"}>
           <DSMAccordion title="Filter">
-            <ForfeituresAdjustmentSearchParameters setInitialSearchLoaded={setInitialSearchLoaded} />
+            <ForfeituresAdjustmentSearchParameters setInitialSearchLoaded={handleSearchComplete} />
           </DSMAccordion>
         </Grid2>
+
+        {showEmployeeDetails && <MasterInquiryEmployeeDetails details={dummyEmployeeDetails} />}
 
         <Grid2 width="100%">
           <ForfeituresAdjustmentGrid
