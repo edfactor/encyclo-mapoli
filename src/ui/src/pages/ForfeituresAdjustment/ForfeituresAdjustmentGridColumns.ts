732c89b9ba04a5b3dba436074a5f03eb004edd8b
Dakota Kim <codingvampire@gmail.com>
2025-04-23 10:33:56 -0400

    no filter

--- src/ui/src/pages/ForfeituresAdjustment/ForfeituresAdjustmentGridColumns.ts
+++ src/ui/src/pages/ForfeituresAdjustment/ForfeituresAdjustmentGridColumns.ts
@@ -7,40 +7,34 @@ export const GetForfeituresAdjustmentColumns = (handleNavigation: (badgeNumber: 
       {
         headerName: "Client #",
         field: "clientNumber",
-        filter: "agTextColumnFilter",
         width: 150
       },
       {
         headerName: "Badge #",
         field: "badgeNumber",
-        filter: "agTextColumnFilter",
         width: 150
       },
       {
         headerName: "Starting Balance",
         field: "startingBalance",
-        filter: "agTextColumnFilter",
         width: 180,
         valueFormatter: agGridNumberToCurrency
       },
       {
         headerName: "Forfeiture Amount",
         field: "forfeitureAmount",
-        filter: "agTextColumnFilter",
         width: 180,
         valueFormatter: agGridNumberToCurrency
       },
       {
         headerName: "Net Balance",
         field: "netBalance",
-        filter: "agTextColumnFilter",
         width: 180,
         valueFormatter: agGridNumberToCurrency
       },
       {
         headerName: "Net Vested",
         field: "netVested",
-        filter: "agTextColumnFilter",
         width: 180,
         valueFormatter: agGridNumberToCurrency
       }
