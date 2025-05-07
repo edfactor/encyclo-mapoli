c6201340ef8d9235c8a2efe810ba02902dc29c98
Russ Harding <rharding@demoulasmarketbasket.com>
2025-05-07 10:47:54 -0400

    Add TypeScript interfaces for health-related data
    Introduced new interfaces: `Health`, `Entries`, `DemoulasCommonDataContext`, `Environment`, `EnvironmentData`, and `MasstransitBus`. These interfaces define the structure and properties for managing health-related information, including attributes like `status`, `duration`, and `tags`.

---
+++ src/ui/src/reduxstore/healthTypes.ts
@@ -0,0 +1,54 @@
+export interface Health {
+  status:        string;
+  totalDuration: string;
+  entries:       Entries;
+}
+
+export interface Entries {
+  OracleHcm:                      DemoulasCommonDataContext;
+  process_allocated_memory:       DemoulasCommonDataContext;
+  "masstransit-bus":              MasstransitBus;
+  ProfitSharingDbContext:         DemoulasCommonDataContext;
+  ProfitSharingReadOnlyDbContext: DemoulasCommonDataContext;
+  DemoulasCommonDataContext:      DemoulasCommonDataContext;
+  self:                           DemoulasCommonDataContext;
+  Environment:                    Environment;
+}
+
+export interface DemoulasCommonDataContext {
+  duration:     string;
+  status:       string;
+  tags:         string[];
+  description?: string;
+}
+
+export interface Environment {
+  data:        EnvironmentData;
+  description: string;
+  duration:    string;
+  status:      string;
+  tags:        any[];
+}
+
+export interface EnvironmentData {
+  Environment:               string;
+  ApplicationName:           string;
+  MachineName:               string;
+  OSVersion:                 string;
+  Framework:                 string;
+  AppVersion:                string;
+  CurrentDirectory:          string;
+  Uptime:                    string;
+  UtcNow:                    Date;
+  OktaEnvironmentName:       string;
+  OktaRolePrefix:            string;
+  OktaAuthorizationServerId: string;
+  OktaAudience:              string;
+}
+
+export interface MasstransitBus {
+  description: string;
+  duration:    string;
+  status:      string;
+  tags:        string[];
+}
