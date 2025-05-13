5f0db946bfbd3ce82ab66f69c40d72bf0b24695a
HassamSherazi <HassamSherazi@HASSAM>
2025-05-13 14:03:57 -0400

    Added test case for Beneficiary List by bade_number and psn_suffix

---
+++ src/services/tests/Demoulas.ProfitSharing.UnitTests.Common/Fakes/BeneficiaryListFaker.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demoulas.ProfitSharing.Data.Entities;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace Demoulas.ProfitSharing.UnitTests.Common.Fakes;
+internal sealed class BeneficiaryListFaker
+{
+    internal List<Beneficiary> GetDummyBeneficiary()
+    {
+        return new List<Beneficiary>()
+        {
+            new Beneficiary()
+            {
+                Id = 144,
+                PsnSuffix = 1000,
+                BadgeNumber = 701218,
+                DemographicId = 1147,
+                Contact = new BeneficiaryContact()
+                {
+                    Id = 144,
+                    Ssn = 700010691,
+                    DateOfBirth = DateOnly.FromDateTime(new DateTime(1966,09,11,0,0,0,DateTimeKind.Utc)),
+                    Address =new Address()
+                    {
+                        Street = "12 PINE PARKWAY",
+                        Street2 = null,
+                        City = "SAUGUS",
+                        State ="NH",
+                        PostalCode = "3729",
+                        CountryIso = "US"
+                    },
+                    ContactInfo = new ContactInfo()
+                    {
+                        FullName ="EVANS, VALENTINA",
+                        LastName = "EVANS",
+                        FirstName = "VALENTINA",
+                        MiddleName =null,
+                        PhoneNumber = null,
+                        MobileNumber = null,
+                        EmailAddress = null
+                    },
+                    CreatedDate = DateOnly.FromDateTime(new DateTime(2025,5,8,0,0,0,DateTimeKind.Utc))
+                },
+                BeneficiaryContactId = 144,
+                Relationship = "HUSBAND",
+                KindId ='P',
+                Kind =new BeneficiaryKind()
+                {
+                    Id = 'P',
+                    Name = "Primary"
+                },
+                Percent = 100
+            },
+            new Beneficiary()
+            {
+                Id = 145,
+                PsnSuffix = 1000,
+                BadgeNumber = 703244,
+                DemographicId = 3173,
+                Contact = new BeneficiaryContact()
+                {
+                    Id = 145,
+                    Ssn = 700010692,
+                    DateOfBirth = DateOnly.FromDateTime(new DateTime(1984,3,4,0,0,0,DateTimeKind.Utc)),
+                    Address =new Address()
+                    {
+                        Street = "243 SECOND COURT",
+                        Street2 = null,
+                        City = "PEPPERELL",
+                        State ="MA",
+                        PostalCode = "2318",
+                        CountryIso = "US"
+                    },
+                    ContactInfo = new ContactInfo()
+                    {
+                        FullName ="DELAROSA, ZOE",
+                        LastName = "DELAROSA",
+                        FirstName = "ZOE",
+                        MiddleName =null,
+                        PhoneNumber = null,
+                        MobileNumber = null,
+                        EmailAddress = null
+                    },
+                    CreatedDate = DateOnly.FromDateTime(new DateTime(2025,5,8,0,0,0,DateTimeKind.Utc))
+                },
+                BeneficiaryContactId = 145,
+                Relationship = "DAUGHTER",
+                KindId ='P',
+                Kind =new BeneficiaryKind()
+                {
+                    Id = 'P',
+                    Name = "Primary"
+                },
+                Percent = 100
+            }
+        };
+    }
+}
---
+++ src/services/tests/Demoulas.ProfitSharing.UnitTests/Endpoints/BeneficiaryInquiry/BeneficiaryServiceTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demoulas.ProfitSharing.Api;
+using Demoulas.ProfitSharing.Common.Interfaces.BeneficiaryInquiry;
+using Demoulas.ProfitSharing.Common.Contracts.Request.BeneficiaryInquiry;
+using Demoulas.ProfitSharing.UnitTests.Common.Base;
+using Microsoft.Extensions.DependencyInjection;
+using Demoulas.ProfitSharing.Common.Contracts.Response.BeneficiaryInquiry;
+using Demoulas.ProfitSharing.Data.Entities;
+using Demoulas.ProfitSharing.Common.Contracts.Response;
+using Demoulas.ProfitSharing.UnitTests.Common.Extensions;
+
+namespace Demoulas.ProfitSharing.UnitTests.Endpoints.BeneficiaryInquiry;
+public class BeneficiaryServiceTest : ApiTestBase<Program>
+{
+    private readonly IBeneficiaryService _beneficiaryService;
+    private readonly List<BeneficiaryDto> _beneficiaryList;
+
+    public BeneficiaryServiceTest()
+    {
+        _beneficiaryService = ServiceProvider?.GetRequiredService<IBeneficiaryService>()!;
+        _beneficiaryList = new List<BeneficiaryDto>()
+        {
+            new BeneficiaryDto()
+            {
+                Id = 145,
+                PsnSuffix = 1000,
+                BadgeNumber = 703244,
+                DemographicId = 3173,
+                Contact = new BeneficiaryContactDto()
+                {
+                    Id = 145,
+                    Ssn = "700010692",
+                    DateOfBirth = DateOnly.FromDateTime(new DateTime(1984,3,4,0,0,0,DateTimeKind.Utc)),
+                    Address =new AddressResponseDto()
+                    {
+                        Street = "243 SECOND COURT",
+                        Street2 = null,
+                        City = "PEPPERELL",
+                        State ="MA",
+                        PostalCode = "2318",
+                        CountryIso = "US"
+                    },
+                    ContactInfo = new ContactInfoResponseDto()
+                    {
+                        FullName ="DELAROSA, ZOE",
+                        LastName = "DELAROSA",
+                        FirstName = "ZOE",
+                        MiddleName =null,
+                        PhoneNumber = null,
+                        MobileNumber = null,
+                        EmailAddress = null
+                    },
+                    CreatedDate = DateOnly.FromDateTime(new DateTime(2025,5,8,0,0,0,DateTimeKind.Utc))
+                },
+                BeneficiaryContactId = 145,
+                Relationship = "DAUGHTER",
+                KindId ='P',
+                Kind =new BeneficiaryKindDto()
+                {
+                    Id = 'P',
+                    Name = "Primary"
+                },
+                Percent = 100
+            }
+        };
+    }
+
+    [Fact(DisplayName ="Get beneficiary by badge_number & psn_suffix")]
+    public async Task GetBeneficiary()
+    {
+        var res = await _beneficiaryService.GetBeneficiary(new BeneficiaryRequestDto() { BadgeNumber = 703244, PsnSuffix = 1000 }, CancellationToken.None);
+        Assert.NotNull(res);
+        res.ShouldBeEquivalentTo(_beneficiaryList);
+    }
+
+
+}
