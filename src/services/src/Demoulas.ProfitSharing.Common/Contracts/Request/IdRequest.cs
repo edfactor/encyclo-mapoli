4677c773b09c90c5656ce0b3d88a29b8281bd1bd
Russ Harding <rharding@demoulasmarketbasket.com>
2025-06-05 09:07:40 -0400

    - Reformat and expand `COPILOT_INSTRUCTIONS.md` for clarity, adding detailed guidelines on coding practices and project organization. - Update `PortHelper.cs` to simplify TCP port check using LINQ and include necessary imports. - Change `IdRequest.cs` from a class to a record type for better data handling. - Improve import statements in `DeleteBeneficiaryContactEndpoint.cs` and `DeleteBeneficiaryEndpoint.cs` for readability.

--- src/services/src/Demoulas.ProfitSharing.Common/Contracts/Request/IdRequest.cs
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Request/IdRequest.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Demoulas.ProfitSharing.Common.Contracts.Request;
-public class IdRequest
+﻿namespace Demoulas.ProfitSharing.Common.Contracts.Request;
+public record IdRequest
 {
     public int Id { get; set; }
 }
