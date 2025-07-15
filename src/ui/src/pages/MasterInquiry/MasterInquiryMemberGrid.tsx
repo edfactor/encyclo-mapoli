eb9918fb48635b89a1a8d9d2a53001869f09bb60
Ed Lyons <ed@eqengineered.com>
2025-07-15 09:31:15 -0400

    Refactor grid columns

--- src/ui/src/pages/MasterInquiry/MasterInquiryMemberGrid.tsx
+++ src/ui/src/pages/MasterInquiry/MasterInquiryMemberGrid.tsx
@@ -1,49 +1,12 @@
-import { Typography } from "@mui/material";
-import React, { useState, useEffect, useRef } from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
 import { useLazySearchProfitMasterInquiryQuery } from "reduxstore/api/InquiryApi";
-import { MasterInquiryRequest, EmployeeDetails } from "reduxstore/types";
+import { EmployeeDetails, MasterInquiryRequest } from "reduxstore/types";
 import { DSMGrid, Pagination, formatNumberWithComma } from "smart-ui-library";
-import { Box, CircularProgress } from "@mui/material";
-import { ColDef } from "ag-grid-community";
 import './MasterInquiryMemberGrid.css'; // Import the CSS file for styles
+import { GetMasterInquiryMemberGridColumns } from "./MasterInquiryMemberGridColumns";
 
-const columns: ColDef[] = [
-  {
-    field: "badgeNumber",
-    headerName: "Badge",
-    maxWidth: 120,
-    cellRenderer: (params: any) => {
-      const { badgeNumber, psnSuffix, isEmployee, id } = params.data;
-      return (
-        <a
-          href="#"
-          className="badge-link"
-          onClick={e => {
-            e.preventDefault();
-            if (params.context && params.context.onBadgeClick) {
-              params.context.onBadgeClick({ memberType: isEmployee ? 1 : 2, id, badgeNumber, psnSuffix });
-            }
-          }}
-        >
-          {psnSuffix > 0 ? `${badgeNumber}-${psnSuffix}` : badgeNumber}
-        </a>
-      );
-    }
-  },
-  { field: "fullName", headerName: "Name", width: 500 },
-  { field: "ssn", headerName: "SSN", maxWidth: 250 },
-  { field: "address", headerName: "Street", maxWidth: 400 },
-  { field: "addressCity", headerName: "City", maxWidth: 300 },
-  { field: "addressState", headerName: "State", maxWidth: 100 },
-  { field: "addressZipCode", headerName: "Zip", maxWidth: 160 },
-  { field: "age", headerName: "Age", maxWidth: 120, },
-  { field: "employmentStatus", headerName: "Status", maxWidth: 120,
-    valueFormatter: (params: any) => {
-      const value = params.value;
-      return value == null || value === undefined || value === "" ? "N/A" : value;
-    }
-  },
-];
+
 
 interface MasterInquiryMemberGridProps extends MasterInquiryRequest {
   onBadgeClick?: (args: { memberType: number; id: number, ssn: number, badgeNumber:number, psnSuffix:number } | undefined) => void;
@@ -100,6 +63,8 @@ const MasterInquiryMemberGrid: React.FC<MasterInquiryMemberGridProps> = (searchP
       </Box>
     );
   }
+
+  const columns = GetMasterInquiryMemberGridColumns();
 
   // Hide the grid if only one member is returned
   if (data && data.results.length === 1) {
--- src/ui/src/pages/MasterInquiry/MasterInquiryMemberGridColumns.ts
+++ src/ui/src/pages/MasterInquiry/MasterInquiryMemberGridColumns.ts
@@ -1,42 +1,28 @@
-import { ColDef } from "ag-grid-community";
-import { agGridNumberToCurrency, formatNumberWithComma } from "smart-ui-library";
+import { ColDef, ICellRendererParams, ValueFormatterParams } from "ag-grid-community";
 
 export const GetMasterInquiryMemberGridColumns = (): ColDef[] => {
-     return [
-  {
-    field: "badgeNumber",
-    headerName: "Badge",
-    maxWidth: 120,
-    cellRenderer: (params: any) => {
-      const { badgeNumber, psnSuffix, isEmployee, id } = params.data;
-      return (
-        <a
-          href="#"
-          className="badge-link"
-          onClick={e => {
-            e.preventDefault();
-            if (params.context && params.context.onBadgeClick) {
-              params.context.onBadgeClick({ memberType: isEmployee ? 1 : 2, id, badgeNumber, psnSuffix });
-            }
-          }}
-        >
-          {psnSuffix > 0 ? `${badgeNumber}-${psnSuffix}` : badgeNumber}
-        </a>
-      );
+    return [
+    {
+        field: "badgeNumber",
+        headerName: "Badge",
+        maxWidth: 120,
+        cellRenderer: (params: ICellRendererParams) => {
+        const { badgeNumber, psnSuffix } = params.data;
+        return psnSuffix > 0 ? `${badgeNumber}-${psnSuffix}` : badgeNumber;
+        }
+    },
+    { field: "fullName", headerName: "Name", maxWidth: 500 },
+    { field: "ssn", headerName: "SSN", maxWidth: 250 },
+    { field: "address", headerName: "Street", maxWidth: 400 },
+    { field: "addressCity", headerName: "City", maxWidth: 300 },
+    { field: "addressState", headerName: "State", maxWidth: 100 },
+    { field: "addressZipCode", headerName: "Zip", maxWidth: 160 },
+    { field: "age", headerName: "Age", maxWidth: 120, },
+    { field: "employmentStatus", headerName: "Status", maxWidth: 120,
+        valueFormatter: (params: ValueFormatterParams) => {
+            const value = params.value;
+            return value == null || value === undefined || value === "" ? "N/A" : value;
+        }
     }
-  },
-  { field: "fullName", headerName: "Name", width: 500 },
-  { field: "ssn", headerName: "SSN", maxWidth: 250 },
-  { field: "address", headerName: "Street", maxWidth: 400 },
-  { field: "addressCity", headerName: "City", maxWidth: 300 },
-  { field: "addressState", headerName: "State", maxWidth: 100 },
-  { field: "addressZipCode", headerName: "Zip", maxWidth: 160 },
-  { field: "age", headerName: "Age", maxWidth: 120, },
-  { field: "employmentStatus", headerName: "Status", maxWidth: 120,
-    valueFormatter: (params: any) => {
-      const value = params.value;
-      return value == null || value === undefined || value === "" ? "N/A" : value;
-    }
-  },
-]
+    ]
 };
