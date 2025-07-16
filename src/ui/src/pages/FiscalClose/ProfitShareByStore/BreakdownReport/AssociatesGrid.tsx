70ba8a64d4ad5015667343dee762c91b1118e404
Ed Lyons <ed@eqengineered.com>
2025-07-16 08:12:00 -0400

    Refactor grid columns

--- src/ui/src/pages/FiscalClose/ProfitShareByStore/BreakdownReport/AssociatesGrid.tsx
+++ src/ui/src/pages/FiscalClose/ProfitShareByStore/BreakdownReport/AssociatesGrid.tsx
@@ -1,20 +1,21 @@
 import { Typography } from "@mui/material";
-import { DSMGrid, ISortParams, Pagination, agGridNumberToCurrency } from "smart-ui-library";
-import { useMemo, useEffect, useState, useCallback, useRef } from "react";
 import Grid2 from "@mui/material/Grid2";
-import { useLazyGetBreakdownByStoreQuery } from "reduxstore/api/YearsEndApi";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
-import { RootState } from "reduxstore/store";
-import { ICellRendererParams } from "ag-grid-community";
-import { viewBadgeLinkRenderer } from "../../../../utils/masterInquiryLink";
 import { useNavigate } from "react-router-dom";
+import { useLazyGetBreakdownByStoreQuery } from "reduxstore/api/YearsEndApi";
+import { RootState } from "reduxstore/store";
+import { DSMGrid, ISortParams, Pagination } from "smart-ui-library";
 import useDecemberFlowProfitYear from "../../../../hooks/useDecemberFlowProfitYear";
+import { GetAssociatesColumns } from "./AssociatesGridColumns";
 
 interface AssociatesGridProps {
   store: number;
+  pageNumberReset: boolean;
+  setPageNumberReset: (reset: boolean) => void;
 }
 
-const AssociatesGrid: React.FC<AssociatesGridProps> = ({ store }) => {
+const AssociatesGrid: React.FC<AssociatesGridProps> = ({ store, pageNumberReset, setPageNumberReset }) => {
   const [pageNumber, setPageNumber] = useState(0);
   const [pageSize, setPageSize] = useState(10);
   const [sortParams, setSortParams] = useState<ISortParams>({
@@ -62,80 +63,14 @@ const AssociatesGrid: React.FC<AssociatesGridProps> = ({ store }) => {
     fetchData();
   }, [fetchData]);
 
-  // Need a useEffect on a change in breakdownByStore to reset the page number
-  const prevBreakdownByStore = useRef<any>(null);
   useEffect(() => {
-    if (breakdownByStore?.response?.results && breakdownByStore.response.results.length > 0 &&
-        (prevBreakdownByStore.current === null || 
-         breakdownByStore.response.results.length !== prevBreakdownByStore.current.response?.results.length)) {
+    if (pageNumberReset) {
       setPageNumber(0);
+      setPageNumberReset(false);
     }
-    prevBreakdownByStore.current = breakdownByStore;
-  } , [breakdownByStore]);
+  }, [pageNumberReset, setPageNumberReset]);
 
-  const columnDefs = useMemo(
-    () => [
-      {
-        headerName: "Badge",
-        field: "badgeNumber",
-        width: 100,
-        cellRenderer: (params: ICellRendererParams) => viewBadgeLinkRenderer(params.data.badgeNumber, handleNavigation)
-      },
-      {
-        headerName: "Name",
-        field: "fullName",
-        width: 200
-      },
-      {
-        headerName: "Position",
-        field: "payClassificationName",
-        width: 120
-      },
-      {
-        headerName: "Beginning Balance",
-        field: "beginningBalance",
-        width: 150,
-        valueFormatter: agGridNumberToCurrency
-      },
-      {
-        headerName: "Earnings",
-        field: "earnings",
-        width: 120,
-        valueFormatter: agGridNumberToCurrency
-      },
-      {
-        headerName: "Contributions",
-        field: "contributions",
-        width: 120,
-        valueFormatter: agGridNumberToCurrency
-      },
-      {
-        headerName: "Forfeiture",
-        field: "forfeitures",
-        width: 120,
-        valueFormatter: agGridNumberToCurrency
-      },
-      {
-        headerName: "Distributions",
-        field: "distributions",
-        width: 120,
-        valueFormatter: agGridNumberToCurrency
-      },
-      {
-        headerName: "Ending Balance",
-        field: "endingBalance",
-        width: 150,
-        valueFormatter: agGridNumberToCurrency
-      },
-      {
-        headerName: "Vested Amount",
-        field: "vestedAmount",
-        width: 150,
-        valueFormatter: agGridNumberToCurrency
-      }
-    ],
-    [handleNavigation]
-  );
+  const columnDefs = useMemo(() => GetAssociatesColumns(handleNavigation), [handleNavigation]);
 
   return (
     <Grid2
