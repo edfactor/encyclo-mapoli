11e8a49ed24766c32c8ee132494eb9dd2aaccb98
Russ Harding <russ@eqengineered.com>
2025-08-07 12:31:40 -0400

    Update navigation mapping and add seed data
    Modified `NavigationMap.cs` to change the `Id` property behavior and added a new `Disabled` property. Introduced seeding for main and sub-menu `Navigation` items. Updated `NavigationRoleMap.cs`, `NavigationStatusMap.cs`, and `NavigationTrackingMap.cs` to include necessary Entity Framework Core namespaces. Added a static `Constants` class in `Navigation.cs` for managing navigation item constants.

--- src/services/src/Demoulas.ProfitSharing.Data/Contexts/EntityMapping/Navigations/NavigationMap.cs
+++ src/services/src/Demoulas.ProfitSharing.Data/Contexts/EntityMapping/Navigations/NavigationMap.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Demoulas.ProfitSharing.Data.Entities;
-using Demoulas.ProfitSharing.Data.Entities.Navigations;
+﻿using Demoulas.ProfitSharing.Data.Entities.Navigations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -15,7 +9,7 @@ internal sealed class NavigationMap : IEntityTypeConfiguration<Navigation>
     {
         _ = builder.ToTable("NAVIGATION");
         _ = builder.HasKey(t => t.Id);
-        _ = builder.Property(x => x.Id).HasColumnName("ID").ValueGeneratedOnAdd();
+        _ = builder.Property(x => x.Id).HasColumnName("ID").ValueGeneratedNever();
         _ = builder.Property(x => x.Url).HasColumnName("URL").HasMaxLength(200);
         _ = builder.Property(x => x.Title).HasColumnName("TITLE").HasMaxLength(100);
         _ = builder.Property(x => x.SubTitle).HasColumnName("SUB_TITLE").HasMaxLength(70);
@@ -23,9 +17,7 @@ internal sealed class NavigationMap : IEntityTypeConfiguration<Navigation>
         _ = builder.Property(x => x.OrderNumber).HasColumnName("ORDER_NUMBER");
         _ = builder.Property(x => x.StatusId).HasColumnName("STATUS_ID");
         _ = builder.Property(x => x.ParentId).HasColumnName("PARENT_ID");
-
-
-
+        _ = builder.Property(x => x.Disabled).HasColumnName("DISABLED");
 
         builder.HasOne(m => m.NavigationStatus)
             .WithMany(x => x.Navigations)
@@ -38,5 +30,132 @@ internal sealed class NavigationMap : IEntityTypeConfiguration<Navigation>
         builder.HasMany(x => x.RequiredRoles)
             .WithMany()
             .UsingEntity(x=>x.ToTable(name:"NAVIGATION_ASSIGNED_ROLES"));
+
+        // Seed data based on add-navigation-data.sql
+        builder.HasData(
+            // Main menu items
+            new Navigation 
+            { 
+                Id = Navigation.Constants.Inquiries, 
+                ParentId = null, 
+                Title = "INQUIRIES", 
+                SubTitle = "", 
+                Url = "", 
+                StatusId = 1, 
+                OrderNumber = 1, 
+                Icon = "", 
+                Disabled = false 
+            },
+            new Navigation 
+            { 
+                Id = Navigation.Constants.Beneficiaries, 
+                ParentId = null, 
+                Title = "BENEFICIARIES", 
+                SubTitle = "", 
+                Url = "", 
+                StatusId = 1, 
+                OrderNumber = 2, 
+                Icon = "", 
+                Disabled = true 
+            },
+            new Navigation 
+            { 
+                Id = Navigation.Constants.Distributions, 
+                ParentId = null, 
+                Title = "DISTRIBUTIONS", 
+                SubTitle = "", 
+                Url = "", 
+                StatusId = 1, 
+                OrderNumber = 3, 
+                Icon = "", 
+                Disabled = true 
+            },
+            new Navigation 
+            { 
+                Id = Navigation.Constants.Reconciliation, 
+                ParentId = null, 
+                Title = "RECONCILIATION", 
+                SubTitle = "", 
+                Url = "", 
+                StatusId = 1, 
+                OrderNumber = 4, 
+                Icon = "", 
+                Disabled = true 
+            },
+            new Navigation 
+            { 
+                Id = Navigation.Constants.YearEnd, 
+                ParentId = null, 
+                Title = "YEAR END", 
+                SubTitle = "", 
+                Url = "", 
+                StatusId = 1, 
+                OrderNumber = 5, 
+                Icon = "", 
+                Disabled = false 
+            },
+            new Navigation 
+            { 
+                Id = Navigation.Constants.ItOperations, 
+                ParentId = null, 
+                Title = "IT OPERATIONS", 
+                SubTitle = "", 
+                Url = "", 
+                StatusId = 1, 
+                OrderNumber = 6, 
+                Icon = "", 
+                Disabled = false 
+            },
+            
+            // Sub-menu items
+            new Navigation 
+            { 
+                Id = Navigation.Constants.MasterInquiry, 
+                ParentId = Navigation.Constants.Inquiries, 
+                Title = "MASTER INQUIRY", 
+                SubTitle = "", 
+                Url = "master-inquiry", 
+                StatusId = 1, 
+                OrderNumber = 1, 
+                Icon = "", 
+                Disabled = false 
+            },
+            new Navigation 
+            { 
+                Id = Navigation.Constants.DemographicFreeze, 
+                ParentId = Navigation.Constants.ItOperations, 
+                Title = "Demographic Freeze", 
+                SubTitle = "", 
+                Url = "demographic-freeze", 
+                StatusId = 1, 
+                OrderNumber = 1, 
+                Icon = "", 
+                Disabled = false 
+            },
+            new Navigation 
+            { 
+                Id = Navigation.Constants.DecemberActivities, 
+                ParentId = Navigation.Constants.YearEnd, 
+                Title = "December Activities", 
+                SubTitle = "", 
+                Url = "december-process-accordion", 
+                StatusId = 1, 
+                OrderNumber = 1, 
+                Icon = "", 
+                Disabled = false 
+            },
+            new Navigation 
+            { 
+                Id = Navigation.Constants.CleanupReports, 
+                ParentId = Navigation.Constants.DecemberActivities, 
+                Title = "Clean up Reports", 
+                SubTitle = "", 
+                Url = "", 
+                StatusId = 1, 
+                OrderNumber = 1, 
+                Icon = "", 
+                Disabled = false 
+            }
+        );
     }
 }
--- src/services/src/Demoulas.ProfitSharing.Data/Entities/Navigations/Navigation.cs
+++ src/services/src/Demoulas.ProfitSharing.Data/Entities/Navigations/Navigation.cs
@@ -1,6 +1,27 @@
 ﻿namespace Demoulas.ProfitSharing.Data.Entities.Navigations;
 public sealed class Navigation
 {
+    public static class Constants
+    {
+        // Main menu items
+        public const int Inquiries = 50;
+        public const int Beneficiaries = 52;
+        public const int Distributions = 53;
+        public const int Reconciliation = 54;
+        public const int YearEnd = 55;
+        public const int ItOperations = 56;
+        
+        // Inquiries sub-items
+        public const int MasterInquiry = 51;
+        
+        // IT Operations sub-items
+        public const int DemographicFreeze = 57;
+        
+        // Year End sub-items
+        public const int DecemberActivities = 1;
+        public const int CleanupReports = 2;
+    }
+
     public int Id { get; set; }
     public int? ParentId { get; set; }
     public required string Title { get; set; }
