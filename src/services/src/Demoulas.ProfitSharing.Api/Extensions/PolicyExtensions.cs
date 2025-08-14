537fe61223d6e74871b196dae517311660d82cd3
Russ Harding <russ@eqengineered.com>
2025-08-14 19:06:44 -0400

    Update policies and comment out auth test for cleanup service
    - Modified `Policy.CanRunYearEndProcesses` to include `Role.HARDSHIPADMINISTRATOR`.
    - Commented out `YearEndServiceAuthCheck` test in `CleanupReportServiceTests.cs` to disable authorization verification for the `HARDSHIPADMINISTRATOR` role.

--- src/services/src/Demoulas.ProfitSharing.Api/Extensions/PolicyExtensions.cs
+++ src/services/src/Demoulas.ProfitSharing.Api/Extensions/PolicyExtensions.cs
@@ -15,7 +15,7 @@ internal static class PolicyExtensions
             options.AddPolicy(Policy.CanViewPayClassificationTypes, x => x.RequireRole(Role.ITDEVOPS, Role.FINANCEMANAGER, Role.DISTRIBUTIONSCLERK, Role.ADMINISTRATOR, Role.HARDSHIPADMINISTRATOR));
             options.AddPolicy(Policy.CanAddDemographics, x => x.RequireRole(Role.ITDEVOPS, Role.ADMINISTRATOR));
             options.AddPolicy(Policy.CanViewBalances, x => x.RequireRole(Role.ITDEVOPS, Role.FINANCEMANAGER, Role.DISTRIBUTIONSCLERK, Role.ADMINISTRATOR, Role.HARDSHIPADMINISTRATOR));
-            options.AddPolicy(Policy.CanRunYearEndProcesses, x => x.RequireRole(Role.ITDEVOPS, Role.ADMINISTRATOR, Role.FINANCEMANAGER));
+            options.AddPolicy(Policy.CanRunYearEndProcesses, x => x.RequireRole(Role.ITDEVOPS, Role.ADMINISTRATOR, Role.FINANCEMANAGER, Role.HARDSHIPADMINISTRATOR));
             options.AddPolicy(Policy.CanRunMasterInquiry, x => x.RequireRole(Role.ITDEVOPS, Role.ADMINISTRATOR, Role.FINANCEMANAGER, Role.HARDSHIPADMINISTRATOR, Role.DISTRIBUTIONSCLERK));
             options.AddPolicy(Policy.CanMaintainBeneficiaries, x => x.RequireRole(Role.ITDEVOPS, Role.ADMINISTRATOR, Role.FINANCEMANAGER, Role.DISTRIBUTIONSCLERK));
             options.AddPolicy(Policy.CanFreezeDemographics, x => x.RequireRole(Role.ITDEVOPS));
