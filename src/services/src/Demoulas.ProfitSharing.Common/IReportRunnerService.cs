f1de02ba84fa045bf93f95233f1126f9e64dbccb
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2025-08-14 08:20:33 -0400

    PS-1527 Add optional report checking to health checks

---
+++ src/services/src/Demoulas.ProfitSharing.Common/IReportRunnerService.cs
@@ -0,0 +1,6 @@
+﻿namespace Demoulas.ProfitSharing.Services.Internal.Interfaces;
+
+public interface IReportRunnerService
+{
+    Task<Dictionary<string, object>> IncludeReportInformation(string reportSelector, CancellationToken cancellationToken);
+}
