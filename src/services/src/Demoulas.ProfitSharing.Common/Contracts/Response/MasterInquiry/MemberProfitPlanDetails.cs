1a5e8f38574b7f4b7bd172bfd17d81165c64d14c
Russ Harding <russ@eqengineered.com>
2025-08-14 17:13:59 -0400

    Update roles and masking attributes across multiple files
    - Changed role references from ITOPERATIONS to ITDEVOPS in PolicyExtensions and SwaggerImpersonationHeader.
    - Updated masking attributes in MasterInquiryResponseDto, MemberDetails, and MemberProfitPlanDetails.
    - Modified role checks in SensitiveValueMaskingMiddleware and test classes.
    - Updated smart-ui-library version in package.json and package-lock.json.

--- src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/MasterInquiry/MemberProfitPlanDetails.cs
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/MasterInquiry/MemberProfitPlanDetails.cs
@@ -1,13 +1,16 @@
-﻿namespace Demoulas.ProfitSharing.Common.Contracts.Response.MasterInquiry;
+﻿using Demoulas.ProfitSharing.Common.Attributes;
+
+namespace Demoulas.ProfitSharing.Common.Contracts.Response.MasterInquiry;
+
 public sealed record MemberProfitPlanDetails : MemberDetails
 {
     public int YearsInPlan { get; init; }
-    public decimal PercentageVested { get; init; }
+    [Unmask] public decimal PercentageVested { get; init; }
     public bool ContributionsLastYear { get; init; }
     public decimal BeginPSAmount { get; set; }
     public decimal CurrentPSAmount { get; set; }
     public decimal BeginVestedAmount { get; set; }
-    public decimal CurrentVestedAmount { get; set;}
+    public decimal CurrentVestedAmount { get; set; }
     public decimal AllocationToAmount { get; set; }
     public decimal AllocationFromAmount { get; set; }
 }
