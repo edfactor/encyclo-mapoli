c13180559a11a4739df23eac6202907265c3c937
Russ Harding <russ@eqengineered.com>
2025-08-15 15:49:55 -0400

    Add [Unmask] attribute and update currency formatting
    - Added [Unmask] attribute to the Name property in NavigationStatusDto.cs for specific value processing.
    - Updated import in gridColumnFactory.ts to include numberToCurrency for currency formatting.
    - Changed valueFormatter to use numberToCurrency instead of agGridNumberToCurrency.
    
    Refactor currency formatting across multiple files
    
    - Added `[Unmask]` attribute to `Name` property in `NavigationStatusDto.cs`.
    - Removed unused import `YearEndProfitSharingReportResponse` in `ProfitShareTotalsDisplay.tsx`.
    - Replaced `agGridNumberToCurrency` with `numberToCurrency` in `TerminationDetailsGridColumns.tsx`, `RehireForfeituresProfitDetailGridColumns.tsx`, `SummariesContent.tsx`, and `ProfitShareGrossReportColumns.ts`.
    - Updated value formatters to use a function for currency formatting.
    - Added `Grid` import in `ReportGrid.tsx`.
    - Cleaned up imports in `gridColumnFactory.ts` to enhance consistency and maintainability.

--- src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/Navigations/NavigationStatusDto.cs
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/Navigations/NavigationStatusDto.cs
@@ -1,6 +1,8 @@
-﻿namespace Demoulas.ProfitSharing.Common.Contracts.Response.Navigations;
+﻿using Demoulas.ProfitSharing.Common.Attributes;
+
+namespace Demoulas.ProfitSharing.Common.Contracts.Response.Navigations;
 public class NavigationStatusDto
 {
     public byte Id { get; set; }
-    public string? Name { get; set; }
+    [Unmask]public string? Name { get; set; }
 }
