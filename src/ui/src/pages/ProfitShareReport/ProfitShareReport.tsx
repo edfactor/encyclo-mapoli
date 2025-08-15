49b36144e3b4bd8137e2e4975c255955229be6a9
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2025-08-15 14:30:56 -0400

    Add clarifing text

--- src/ui/src/pages/ProfitShareReport/ProfitShareReport.tsx
+++ src/ui/src/pages/ProfitShareReport/ProfitShareReport.tsx
@@ -188,6 +188,46 @@ const ProfitShareReport = () => {
         ]}
         title="Are you ready to Commit?">
         Committing this change will update and save:
+
+          <div>
+              <table cellPadding={20} style={{width: "100%"}}>
+                  <tr>
+                      <td>
+                          Earn Points
+                      </td>
+                      <td>
+                          How much money goes towards allocating a contribution
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>ZeroContributionReason</td>
+                      <td> Why did an employee get a zero contribution? Normal,
+                          Under21, Terminated
+                          (Vest Only), Retired, Soon to be Retired
+                      </td>
+                  </tr>
+
+                  <tr>
+                      <td>EmployeeType</td>
+                      <td> Is this a "new employee in the plan" - aka this is your first
+                          year &gt;21
+                          and &gt;1000 hours - employee may already have V-ONLY records
+
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>PsCertificateIssuedDate</td>
+                      <td> indicates that this employee should get a
+                          physically printed
+                          certificate. It is a proxy for Earn Points &gt; 0.
+                      </td>
+                  </tr>
+              </table>
+          </div>
+          <div>
+              This "COMMIT" is safe to run multiple times, until the "Master Update" is saved.  Running "COMMIT" after Master Update means that the Master Update will potentially not have computed the correct earnings and contribution amounts.
+          </div>
+          
       </SmartModal>
     </Page>
   );
