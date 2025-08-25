ea8323220e113d6e8dff199408b59281ae0f2461
HassamSherazi <HassamSherazi@HASSAM>
2025-08-25 16:05:16 -0400

    Added more test-cases in master inquiry

--- src/ui/e2e/master-inquiry/master-inquiry-page.test.ts
+++ src/ui/e2e/master-inquiry/master-inquiry-page.test.ts
@@ -13,17 +13,67 @@ test.describe("Master Inqiry landing page: ", () => {
         await expect(page.getByRole("heading", { name: "MASTER INQUIRY (008-10)" })).toBeVisible();
     });
 
-    test("click on search button without setting IMPERSONATE value", async ({page}) => {
-        await page.getByRole("button",{name:"SEARCH"}).click();
+    test("click on search button without setting IMPERSONATE value", async ({ page }) => {
+        await page.getByRole("button", { name: "SEARCH" }).click();
         await page.waitForLoadState("networkidle");
-        await expect(page.getByRole("paragraph",{name:"Search Failed"})).toBeVisible();
+        await expect(page.getByText("Search Failed")).toBeVisible();
     });
 
-    test("click on search button after setting IMPERSONATE value", async ({page}) => {
-        await page.getByRole("combobox",{name:"roles"}).click();
+    test("click on search button after setting IMPERSONATE value", async ({ page }) => {
+        await page.getByRole("combobox", { name: "roles" }).click();
         await page.getByRole('option', { name: 'Finance-Manager' }).getByRole('checkbox').check();
-        await page.getByRole("button",{name:"SEARCH"}).dblclick();
-        await page.waitForLoadState("networkidle");
-        await expect(page.getByRole("presentation",{name:"Badge"}).isVisible())
+        await page.locator("body").click();
+        await page.getByRole("button", { name: "SEARCH" }).dblclick();
+        const [response] = await Promise.all([page.waitForResponse((resp) =>
+            resp.url().includes('master-inquiry/search'))]);
+        await expect(response.status()).toBe(200);
+    });
+
+    test("pagingation is working properly or not", async ({ page }) => {
+        await page.getByRole("combobox", { name: "roles" }).click();
+        await page.getByRole('option', { name: 'Finance-Manager' }).getByRole('checkbox').check();
+        await page.locator("body").click();
+        await page.getByRole("button", { name: "SEARCH" }).dblclick();
+        const [response] = await Promise.all([page.waitForResponse((resp) =>
+            resp.url().includes('master-inquiry/search'))]);
+        await expect(response.status()).toBe(200);
+        await page.getByRole('button', { name: 'next page' }).click();
+        const [response1] = await Promise.all([page.waitForResponse((resp) =>
+            resp.url().includes('master-inquiry/search'))]);
+        const json = await response1.json();
+        await expect(json.results.length).toBeGreaterThan(0);
+        
+    });
+    test("enter Name and click search", async ({ page }) => {
+        await page.getByRole("combobox", { name: "roles" }).click();
+        await page.getByRole('option', { name: 'Finance-Manager' }).getByRole('checkbox').check();
+        await page.locator("body").click();
+        await page.locator('input[name="name"]').fill("evans");
+        await page.getByRole("button", { name: "SEARCH" }).dblclick();
+        const [response] = await Promise.all([page.waitForResponse((resp) =>
+            resp.url().includes('master-inquiry/search'))]);
+        await expect(response.status()).toBe(200);
+    });
+
+    test("enter Badge/Psn Number and click search", async ({ page }) => {
+        await page.getByRole("combobox", { name: "roles" }).click();
+        await page.getByRole('option', { name: 'Finance-Manager' }).getByRole('checkbox').check();
+        await page.locator("body").click();
+        await page.locator('input[name="badgeNumber"]').fill("706056")
+        await page.getByRole("button", { name: "SEARCH" }).dblclick();
+        const [response] = await Promise.all([page.waitForResponse((resp) =>
+            resp.url().includes('master-inquiry/search'))]);
+        await expect(response.status()).toBe(200);
+    });
+
+    test("select Payment Type  and click search", async ({ page }) => {
+        await page.getByRole("combobox", { name: "roles" }).click();
+        await page.getByRole('option', { name: 'Finance-Manager' }).getByRole('checkbox').check();
+        await page.locator("body").click();
+        await page.getByRole('radio', { name: 'Payoffs/Forfeit' }).check();
+        await page.getByRole("button", { name: "SEARCH" }).dblclick();
+        const [response] = await Promise.all([page.waitForResponse((resp) =>
+            resp.url().includes('master-inquiry/search'))]);
+        await expect(response.status()).toBe(200);
     });
 });
