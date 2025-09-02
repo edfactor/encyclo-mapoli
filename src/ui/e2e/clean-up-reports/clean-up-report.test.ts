eed1bd36fbfbc96933d8a1ff5ed38a87f6c72699
HassamSherazi <HassamSherazi@HASSAM>
2025-09-02 12:40:52 -0400

    added all pages for test case

--- src/ui/e2e/clean-up-reports/clean-up-report.test.ts
+++ src/ui/e2e/clean-up-reports/clean-up-report.test.ts
@@ -6,20 +6,42 @@ test.describe("Master Inqiry landing page: ", () => {
     test.beforeEach(async ({ page }) => {
         await page.goto(baseUrl);
         await page.waitForLoadState("networkidle");
-        await page.getByRole("button", { name: "INQUIRIES" }).click();
-        await page.getByRole("link", { name: "MASTER INQUIRY" }).click();
         await page.getByRole("combobox", { name: "roles" }).click();
         await page.getByRole('option', { name: 'Finance-Manager' }).getByRole('checkbox').check();
         await page.locator("body").click();
-    });
-
-    test('Demographic Badges Not In PayProfit',async ({page})=>{
+        await page.reload();
+        await page.waitForLoadState("networkidle");
         await page.getByRole('button').filter({ hasText: /^$/ }).click();
         await page.getByRole('button', { name: 'December Activities' }).click();
         await page.getByRole('button', { name: 'Clean up Reports' }).click();
+    });
+
+    test('Demographic Badges Not In PayProfit',async ({page})=>{
         await page.getByRole('button', { name: 'Demographic Badges Not In PayProfit' }).click();
         const [response] = await Promise.all([page.waitForResponse((resp) =>
             resp.url().includes('yearend/demographic-badges-not-in-payprofit'))]);
+        await expect(response.status()).toBe(200);
+    });
+
+    test('Duplicate SSNs in Demographics',async ({page})=>{
+        await page.getByRole('button', { name: 'Duplicate SSNs in Demographics' }).click();
+        const [response] = await Promise.all([page.waitForResponse((resp) =>
+            resp.url().includes('yearend/duplicate-ssns'))]);
+        await expect(response.status()).toBe(200);
+    });
+
+    
+    test('Negative ETVA',async ({page})=>{
+        await page.getByRole('button', { name: 'Negative ETVA' }).click();
+        const [response] = await Promise.all([page.waitForResponse((resp) =>
+            resp.url().includes('yearend/negative-evta-ssn'))]);
+        await expect(response.status()).toBe(200);
+    });
+
+    test('Duplicate Names and Birthdays',async ({page})=>{
+        await page.getByRole('button', { name: 'Duplicate Names and Birthdays' }).click();
+        const [response] = await Promise.all([page.waitForResponse((resp) =>
+            resp.url().includes('yearend/duplicate-names-and-birthdays'))]);
         await expect(response.status()).toBe(200);
     });
 
