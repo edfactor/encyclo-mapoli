3ea48da54fe0dbdb7e16d56529ea37970f1aa5b5
Ed Lyons <ed@eqengineered.com>
2025-09-08 15:42:42 -0400

    Fix it devops breadcrumb name

--- src/ui/src/utils/getReadablePathName.ts
+++ src/ui/src/utils/getReadablePathName.ts
@@ -69,7 +69,8 @@ const pathToReadableName: Record<string, string> = {
   qpay600: "QPAY600",
   "reprint-certificates": "Reprint Certificates",
   "print-profit-certs": "Print Profit Certificates",
-  "recently-terminated": "Recently Terminated"
+  "recently-terminated": "Recently Terminated",
+  "it devops": "IT DevOps"
 };
 
 export const getReadablePathName = (path: string): string => {
