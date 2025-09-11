20061fe24dc4d01fb0be2a1546ade37f060e8830
Russ Harding <russ@eqengineered.com>
2025-09-11 15:58:53 -0400

    Logging package update

--- src/services/src/Demoulas.ProfitSharing.Data/Entities/Virtual/ParticipantTotalYear.cs
+++ src/services/src/Demoulas.ProfitSharing.Data/Entities/Virtual/ParticipantTotalYear.cs
@@ -1,5 +1,5 @@
 ﻿namespace Demoulas.ProfitSharing.Data.Entities.Virtual;
-public class ParticipantTotalYear
+public sealed class ParticipantTotalYear
 {
     public int Ssn { get; set; }
     public byte Years { get; set; }
