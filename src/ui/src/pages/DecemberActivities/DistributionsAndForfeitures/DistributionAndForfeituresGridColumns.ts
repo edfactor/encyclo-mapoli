71105f06a44a5be9905841e38359046572e8090c
Ed Lyons <ed@eqengineered.com>
2025-09-12 09:52:39 -0400

    Clean up grid columns

--- src/ui/src/pages/DecemberActivities/DistributionsAndForfeitures/DistributionAndForfeituresGridColumns.ts
+++ src/ui/src/pages/DecemberActivities/DistributionsAndForfeitures/DistributionAndForfeituresGridColumns.ts
@@ -1,5 +1,4 @@
 import { ColDef } from "ag-grid-community";
-import { GRID_COLUMN_WIDTHS } from "../../../constants";
 import { getEnrolledStatus } from "../../../utils/enrollmentUtil";
 import {
   createAgeColumn,
@@ -16,49 +15,39 @@ export const GetDistributionsAndForfeituresColumns = (): ColDef[] => {
   return [
     createBadgeColumn({
       headerName: "Badge",
-      minWidth: GRID_COLUMN_WIDTHS.BADGE_NUMBER,
-      alignment: "left",
+
       psnSuffix: true
     }),
     createNameColumn({
       field: "employeeName",
       minWidth: 150
     }),
-    createSSNColumn({ alignment: "left" }),
+    createSSNColumn({}),
     createDateColumn({
       headerName: "Date",
-      field: "date",
-      minWidth: 100,
-      alignment: "left"
+      field: "date"
     }),
     createCurrencyColumn({
       headerName: "Distribution",
-      field: "distributionAmount",
-      minWidth: 120
+      field: "distributionAmount"
     }),
     createCurrencyColumn({
       headerName: "State Tax",
-      field: "stateTax",
-      minWidth: 120
+      field: "stateTax"
     }),
-    createStateColumn({
-      minWidth: 120,
-      alignment: "left"
-    }),
+    createStateColumn({}),
     createCurrencyColumn({
       headerName: "Federal Tax",
-      field: "federalTax",
-      minWidth: 120
+      field: "federalTax"
     }),
     createCurrencyColumn({
       headerName: "Forfeit Amount",
-      field: "forfeitAmount",
-      minWidth: 120
+      field: "forfeitAmount"
     }),
     createAgeColumn({}),
     createTaxCodeColumn({
       field: "taxCode",
-      minWidth: 80,
+
       alignment: "right",
       showBrackets: false,
       hideZeroValues: false
@@ -67,7 +56,7 @@ export const GetDistributionsAndForfeituresColumns = (): ColDef[] => {
       headerName: "Enrolled",
       field: "enrolledId",
       colId: "enrolledId",
-      minWidth: 90,
+
       headerClass: "center-align",
       cellClass: "center-align",
       resizable: true,
