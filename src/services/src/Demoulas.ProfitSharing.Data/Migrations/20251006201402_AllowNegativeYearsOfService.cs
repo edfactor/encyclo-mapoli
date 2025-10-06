3e78366d7c3883518af325cbe9f79644b84c5baa
Phil Denoncourt <pdenoncourt@demoulasmarketbaseket.com>
2025-10-06 16:26:09 -0400

    Add migration to allow negative years.

---
+++ src/services/src/Demoulas.ProfitSharing.Data/Migrations/20251006201402_AllowNegativeYearsOfService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Demoulas.ProfitSharing.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowNegativeYearsOfService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.AlterColumn<short>(
+                name: "YEARS_OF_SERVICE_CREDIT",
+                table: "PROFIT_DETAIL",
+                type: "NUMBER(3)",
+                nullable: false,
+                defaultValue: (short)0,
+                oldClrType: typeof(byte),
+                oldType: "NUMBER(3)",
+                oldDefaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "YEARS_OF_SERVICE_CREDIT",
+                table: "PROFIT_DETAIL",
+                type: "NUMBER(3)",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(short),
+                oldType: "NUMBER(3)",
+                oldDefaultValue: (short)0);
+
+        }
+    }
+}
