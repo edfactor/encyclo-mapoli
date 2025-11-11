1f5938fba8968de53c23a71a2b781f817236be11
Russ Harding <russ@eqengineered.com>
2025-11-10 20:35:18 -0500

    Revert "Remove `ProfitSharingUnder21ReportTests` class"
    This reverts commit 2d0da40c2429b49702670d30859768270b7f5689.

---
+++ src/services/tests/Demoulas.ProfitSharing.UnitTests/Reports/PostFrozen/ProfitSharingUnder21ReportTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using Demoulas.ProfitSharing.Api;
+using Demoulas.ProfitSharing.Common.Contracts.Request;
+using Demoulas.ProfitSharing.Common.Contracts.Response.PostFrozen;
+using Demoulas.ProfitSharing.Endpoints.Endpoints.Reports.YearEnd.PostFrozen;
+using Demoulas.ProfitSharing.Security;
+using Demoulas.ProfitSharing.UnitTests.Common.Base;
+using Demoulas.ProfitSharing.UnitTests.Common.Extensions;
+using FastEndpoints;
+using Shouldly;
+
+namespace Demoulas.ProfitSharing.UnitTests.Reports.PostFrozen;
+
+public sealed class ProfitSharingUnder21ReportTests : ApiTestBase<Program>
+{
+    [Fact(DisplayName = "PS-419 Check Under 21 Report")]
+    public async Task CheckUnder21Report()
+    {
+        var request = new ProfitYearRequest() { ProfitYear = 2024, Skip = 0, Take = 255 };
+        var response = await ApiClient.GETAsync<ProfitSharingUnder21ReportEndpoint, ProfitYearRequest, ProfitSharingUnder21ReportResponse>(request);
+
+        response.ShouldNotBeNull();
+        response.Response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+
+        ApiClient.CreateAndAssignTokenForClient(Role.ADMINISTRATOR, Role.EXECUTIVEADMIN);
+        response = await ApiClient.GETAsync<ProfitSharingUnder21ReportEndpoint, ProfitYearRequest, ProfitSharingUnder21ReportResponse>(request);
+        response.ShouldNotBeNull();
+        response.Response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+
+}
