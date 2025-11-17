1a823704a141381449443308b3e204a4f1eaa14a
Ed Lyons <ed@eqengineered.com>
2025-11-17 18:51:22 -0500

    Fix BreakdownByStoreEmployee core type

--- src/ui/src/types/reports/breakdown.ts
+++ src/ui/src/types/reports/breakdown.ts
@@ -9,24 +9,34 @@ export interface BreakdownByStoreRequest extends ProfitYearRequest {
 }
 
 export interface BreakdownByStoreEmployee {
-  storeNumber: number;
-  enrollmentId: number;
   badgeNumber: number;
-  ssn: string;
-  fullName: string;
-  payFrequencyId: number;
-  departmentId: number;
-  payClassificationId: string; // changed from number to string to match backend refactor
   beginningBalance: number;
-  earnings: number;
+  certificateSort: number;
+  city: string;
   contributions: number;
-  forfeitures: number;
+  dateOfBirth: string;
+  departmentId: number;
   distributions: number;
+  earnings: number;
+  employmentStatusId: string;
   endingBalance: number;
+  enrollmentId: number;
+  forfeitures: number;
+  fullName: string;
+  hireDate: string;
+  isExecutive: boolean;
+  payClassificationId: string; // changed from number to string to match backend refactor
+  payClassificationName: string;
+  payFrequencyId: number;
+  postalCode: string;
+  profitShareHours: number;
+  ssn: string;
+  state: string;
+  storeNumber: number;
+  street1: string;
+  terminationDate: string;
   vestedAmount: number;
   vestedPercentage: number;
-  employmentStatusId: string;
-  payClassificationName: string;
 }
 
 export interface BreakdownByStoreTotals {
