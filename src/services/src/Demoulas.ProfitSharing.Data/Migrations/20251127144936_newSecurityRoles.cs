e8be4f1e2ef955e3f60c53c2a438e993c94a55b2
Russ Harding <russ@eqengineered.com>
2025-11-27 09:50:51 -0500

    Add new security roles and update YEARS_OF_SERVICE_CREDIT column type
    - Altered the column type of YEARS_OF_SERVICE_CREDIT in the PROFIT_DETAIL table from byte to short with a default value of 0.
    - Inserted new navigation roles: "HR-ReadOnly" and "SSN-Unmasking" into the NAVIGATION_ROLE table.
    - Implemented a rollback mechanism to remove the inserted roles and revert the column type change.

---
+++ src/services/src/Demoulas.ProfitSharing.Data/Migrations/20251127144936_newSecurityRoles.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Demoulas.ProfitSharing.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class newSecurityRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "YEARS_OF_SERVICE_CREDIT",
+                table: "PROFIT_DETAIL",
+                type: "NUMBER(3)",
+                nullable: false,
+                defaultValue: (short)0,
+                oldClrType: typeof(byte),
+                oldType: "NUMBER(3)",
+                oldDefaultValue: (byte)0);
+
+            migrationBuilder.InsertData(
+                table: "NAVIGATION_ROLE",
+                columns: new[] { "ID", "IS_READ_ONLY", "NAME" },
+                values: new object[,]
+                {
+                    { (byte)11, true, "HR-ReadOnly" },
+                    { (byte)12, true, "SSN-Unmasking" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "NAVIGATION_ROLE",
+                keyColumn: "ID",
+                keyValue: (byte)11);
+
+            migrationBuilder.DeleteData(
+                table: "NAVIGATION_ROLE",
+                keyColumn: "ID",
+                keyValue: (byte)12);
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "YEARS_OF_SERVICE_CREDIT",
+                table: "PROFIT_DETAIL",
+                type: "NUMBER(3)",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(short),
+                oldType: "NUMBER(3)",
+                oldDefaultValue: (short)0);
+        }
+    }
+}
