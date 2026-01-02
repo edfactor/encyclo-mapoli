c8e7ae7289d834081fdd88a2efc47e1849c729a1
Russ Harding <russ@eqengineered.com>
2026-01-02 15:55:35 -0500

    Remove unused variables for current year and month in ReversalsGridColumns tests

--- src/ui/src/pages/InquiriesAndAdjustments/Reversals/__test__/ReversalsGridColumns.test.ts
+++ src/ui/src/pages/InquiriesAndAdjustments/Reversals/__test__/ReversalsGridColumns.test.ts
@@ -28,9 +28,6 @@ describe("ReversalsGridColumns", () => {
       vi.useRealTimers();
     });
 
-    const currentYear = 2025;
-    const currentMonth = 6; // June
-
     describe("null/undefined data", () => {
       it("should return ineligible for null data", () => {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -277,9 +274,6 @@ describe("ReversalsGridColumns", () => {
     afterEach(() => {
       vi.useRealTimers();
     });
-
-    const currentYear = 2025;
-    const currentMonth = 6; // June
 
     it("should return default message for null data", () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
