7d26d5436721583007dcfbca4e09aeaadd725fa8
Russ Harding <russ@eqengineered.com>
2026-01-13 18:16:27 -0500

    Add TotalVestedBalance to account history report totals
    Added TotalVestedBalance to AccountHistoryReportTotals, calculated as the EndingBalance of the latest ProfitYear in AccountHistoryReportService. Also included minor formatting updates and additional using statements.

--- src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/AccountHistoryReportTotals.cs
+++ src/services/src/Demoulas.ProfitSharing.Common/Contracts/Response/AccountHistoryReportTotals.cs
@@ -1,4 +1,4 @@
-namespace Demoulas.ProfitSharing.Common.Contracts.Response;
+﻿namespace Demoulas.ProfitSharing.Common.Contracts.Response;
 
 /// <summary>
 /// Cumulative totals for Account History Report
